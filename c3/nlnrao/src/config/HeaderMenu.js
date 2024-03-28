a05cdbfc08c3b1951095b91703bcc742b6930d23
Nellutla L N Rao <nlnrao228@gmail.com>
2024-03-28 21:53:01 +0530

    Updated Projects

--- c3/nlnrao/src/config/HeaderMenu.js
+++ c3/nlnrao/src/config/HeaderMenu.js
@@ -1,3 +1,3 @@
-export const HeaderMenu = [{ id:'Home', url: 'home', label:'Home' },
+export const HeaderMenu = [{ id:'Home', url: '', label:'Home' },
 { id:'SystemDesignIntroduction', url:'system-design/introduction', label:'System Design' },
 { id:'BlockchainIntroduction', url:'blockchain/introduction', label:'Blockchain' }];
