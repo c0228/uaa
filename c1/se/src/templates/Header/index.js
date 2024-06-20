377e52fa8be9d5321d9313da390722d6844caf2e
github-tejaswi <tkukalakunta@gmail.com>
2024-06-20 23:19:36 +0530

    Updated Project

--- c1/se/src/templates/Header/index.js
+++ c1/se/src/templates/Header/index.js
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Icon } from "e-ui-react";
 import { Link } from 'react-router-dom';
 import './index.css';
 
 const Header = ({ activeId, menulinks })=>{
  const [showCollapse, setShowCollapse] = useState(false);
+ useEffect(()=>{
+  document.body.style.backgroundColor = '#fff';
+ },[]);
  return (<>
  <div className="upper-header">
     <div className="upper-header-flex">
