adab456d2c8c0dac1537b2db861fe27ac1ffd924
n0228 <nlnrao228@gmail.com>
2024-09-09 21:27:33 +0530

    Updated Projects

--- c3/vkap/src/pages/MyProfile/components/basic-details/index.js
+++ c3/vkap/src/pages/MyProfile/components/basic-details/index.js
@@ -1,10 +1,29 @@
 import React, { useState, useEffect } from "react";
-import { TextBox, Form, FormToReqBodyFormatter, UrlAsyncFetch } from "e-ui-react";
+import { TextBox, Form, Alert, Icon, FormToReqBodyFormatter, UrlAsyncFetch } from "e-ui-react";
 
 const BasicDetails = () =>{
- const [showAlert, setShowAlert] = useState();
- useEffect(()=>{ },[]);
+ const [showAlert, setShowAlert] = useState({ type:'', icon:'', show: false, message:'' });
+ const [data, setData] = useState({
+    userId: '',
+    name: '',
+    email: '',
+    mobile: ''
+ });
+ const initialize = () =>{
+    let userAuthDetails = localStorage.getItem("USER_AUTH_DETAILS");
+        userAuthDetails = JSON.parse(userAuthDetails);
+    setData({ userId: userAuthDetails?.data?.userId, name: userAuthDetails?.data?.name, 
+        email: userAuthDetails?.data?.email, mobile: userAuthDetails?.data?.mobile
+     });
+ };
+ useEffect(()=>{ initialize(); },[]);
  return (<div>
+    {showAlert?.show && (<div className="mbot15p">
+          <Alert type={showAlert?.type} show={showAlert?.show} body={<div>
+              <Icon type="FontAwesome" name={showAlert?.icon} size={16} />
+              <span className="mLeft5p">{showAlert?.message}</span>
+            </div>} />
+        </div>)}
     <Form name="BasicDetailsForm" btnSubmit={{
         align: 'center',
         btnType:'success',
@@ -20,31 +39,37 @@ const BasicDetails = () =>{
     onSubmit={async(form, isValidForm, triggerReset)=>{
         if(isValidForm){  
             // Error Validation check
-            let postData = FormToReqBodyFormatter(form.AddEmployeeForm); 
-            const response = await UrlAsyncFetch( process.env.NEXUS_URL + 'user/register', 
+            let postData = FormToReqBodyFormatter(form.BasicDetailsForm); 
+                postData.userId = data?.userId;
+            const response = await UrlAsyncFetch( process.env.NEXUS_URL + 'user/details/update', 
                 'POST', postData );
             console.log("logicResposne", response);
             if(response?.status?.toLowerCase()==='success'){
-                setShowAlert({ type:'success', icon:'fa-check-circle', show: true, message:'New User Account created Successfully' });
+                setShowAlert({ type:'success', icon:'fa-check-circle', show: true, message:'Updated your User Profile Successfully' });
                 initialize();
+                // Update to localStorage
+                let userAuthDetails = localStorage.getItem("USER_AUTH_DETAILS");
+                    userAuthDetails = JSON.parse(userAuthDetails);
+                    userAuthDetails.data = response?.data?.[0];
+                localStorage.setItem("USER_AUTH_DETAILS", JSON.stringify(userAuthDetails));
+                window.dispatchEvent(new Event("storage"));
             } else {
-                setShowAlert({ type:'danger', icon:'warning', show: true, message:'Failed to create New User Account' });
+                setShowAlert({ type:'danger', icon:'warning', show: true, message:'Failed to update your User Profile' });
             }
-            triggerReset();
         }
     }}
     onReset={async(triggerReset)=>{
         triggerReset();
     }}>
-    <div>
-        <TextBox name="name" label="Your Name" placeholder="Enter your Name" />
-    </div>
-    <div className="mtop15p">
-        <TextBox name="email" label="Email Address" placeholder="Enter Email Address" />
-    </div>
-    <div className="mtop15p">
-        <TextBox name="mobile" label="Mobile Number (Without +91)" placeholder="Enter Mobile Number" />
-    </div>
+    {data?.name && (<div>
+        <TextBox name="name" label="Your Name" placeholder="Enter your Name" value={data?.name} />
+    </div>)}
+    {data?.email && (<div className="mtop15p">
+        <TextBox name="email" label="Email Address" placeholder="Enter Email Address" value={data?.email} />
+    </div>)}
+    {data?.mobile && (<div className="mtop15p">
+        <TextBox name="mobile" label="Mobile Number (Without +91)" placeholder="Enter Mobile Number" value={data?.mobile} />
+    </div>)}
     </Form>
  </div>);
 };
--- c3/vkap/src/templates/Header/index.js
+++ c3/vkap/src/templates/Header/index.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Nav, Button, Icon, DropdownElement } from "e-ui-react";
 import { useAuth } from "@Provider/AuthProvider";
 import './index.css';
 
 const Header = ({ activeId, menulinks })=>{
-
+ const [userName, setUserName] = useState();
  const { logout } = useAuth();
 
- let userAuthDetails = localStorage?.getItem("USER_AUTH_DETAILS"); 
-     userAuthDetails = JSON.parse( userAuthDetails );
+ const initialize = () =>{
+  let userAuthDetails = localStorage?.getItem("USER_AUTH_DETAILS"); 
+  userAuthDetails = JSON.parse( userAuthDetails );
+  setUserName( userAuthDetails?.data?.name );
+ };
 
- const name = userAuthDetails?.data?.name;
+ useEffect(()=>{
+  initialize();
+ },[]);
+
+ window.addEventListener('storage', () => {
+  initialize();
+ });
 
  const NavHeaderRight = () =>{
 
@@ -20,11 +29,12 @@ const Header = ({ activeId, menulinks })=>{
   };
 
   const NavLabel = () =>{
-    return (<div align="right"><b>Hello <span className="mRight5p">{name}</span></b> 
+    return (<div align="right"><b>Hello <span className="mRight5p">{userName}</span></b> 
     <Icon type="FontAwesome" name="fa-caret-down" size={12} /></div>);
   };
 
-  const menus = [{ url: process.env.PROJECT_URL+'consultancy/my-profile', title:(<div><NavMenu icon="fa-user" title="My Profile" /></div>) },
+  const menus = [{ url: process.env.PROJECT_URL+'consultancy/my-profile', 
+            title:(<div><NavMenu icon="fa-user" title="My Profile" /></div>) },
     { url:'#', title:(<div onClick={()=>logout('/')}><NavMenu icon="fa-sign-out" title="Logout" /></div>) }];
 
   return (<DropdownElement width="160px" label={<NavLabel />} menus={menus} />);
