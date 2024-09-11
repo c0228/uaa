6db59700a6fe4d87040b1eb4c8ad1322d46844e9
n0228 <nlnrao228@gmail.com>
2024-09-11 16:45:35 +0530

    Updated Projects

--- c3/vkap/src/pages/UploadData/index.js
+++ c3/vkap/src/pages/UploadData/index.js
@@ -1,13 +1,16 @@
-import React, { useEffect } from "react";
-import { ContainerFluid, Row, Col, Card, Button, Icon, FileUpload, UrlAsyncFetch  } from "e-ui-react";
+import React, { useState, useEffect } from "react";
+import { ContainerFluid, Row, Col, Card, Button, Icon, FileUpload, UrlAsyncFetch, Colors } from "e-ui-react";
 import Header from '@Templates/Header/index.js';
 import Footer from '@Templates/Footer/index.js';
 import { HeaderMenu } from '@Routes/NavbarList.js';
+import './index.css';
 
 const UploadData = () =>{
+ const [uploadedList, setUploadedList] = useState([]);
  useEffect(()=>{
   document.title = 'Upload Data | VKAbroad';
   document.body.style.backgroundColor = "#fcfcfc";
+  displayUploadFilesList();
  },[]);
  const HeaderTitle = ({ title }) =>{
   return (<div style={{ display:'flex', flexDirection:'row', marginBottom:'5px', borderBottom:'1px solid #ccc' }}>
@@ -18,6 +21,16 @@ const UploadData = () =>{
     return (<div align="center" style={{ textTransform:'uppercase', paddingBottom:'15px' }}>
       <b>{title}</b>
     </div>);
+ };
+ const bulkUploadExecution = async() =>{
+  const response = await UrlAsyncFetch( process.env.NEXUS_URL + 'university/bulk/upload', 
+    'POST', { targetDirectory:'/', fileName:files  } );
+  console.log("response", response);
+ };
+ const displayUploadFilesList = async() =>{
+  const response = await UrlAsyncFetch( process.env.NEXUS_URL + 'university/files/list', 'GET', { } );
+  console.log("response [displayUploadFilesList]: ", response);
+  setUploadedList(response);
  };
  return (<div>
   <Header menulinks={HeaderMenu()} activeId="UploadData" />
@@ -31,7 +44,7 @@ const UploadData = () =>{
         <Col md={6}>
          <Card padding={15}>
           <SubHeaderTitle title="Upload Excel Sheet" />
-          <div style={{ minHeight:'40vh' }}>
+          <div style={{ minHeight:'43.2vh' }}>
             <FileUpload name="helloWorld" type="fileDropper" height="240" autoSubmit={true} 
               backend={{
                 "apiUrl": process.env.NEXUS_URL+"upload/file",
@@ -41,9 +54,8 @@ const UploadData = () =>{
                 console.log("afterUploadSuccess [fileUploaderDetails]: "+fileUploaderDetails);
                 const uploaderDetails = JSON.parse(fileUploaderDetails);
                 const files = uploaderDetails?.map((detail)=>detail?.name);
-                const response = await UrlAsyncFetch( process.env.NEXUS_URL + 'university/bulk/upload', 
-                  'POST', { targetDirectory:'/', fileName:files  } );
-                console.log("response", response);
+                bulkUploadExecution(files);
+                displayUploadFilesList();
                 // university/bulk/upload
                 // targetDirectory
                 // fileName
@@ -55,7 +67,7 @@ const UploadData = () =>{
         <Col md={6}>
          <Card padding={15}>
           <SubHeaderTitle title="Excel Sheet Template" />
-          <div style={{ height:'41.6vh' }}>
+          <div style={{ height:'45vh' }}>
             <div><b>Instructions:</b></div>
             <div>
                 <ul>
@@ -70,7 +82,8 @@ const UploadData = () =>{
                         If you adding new University Data, make sure University Id given by you in AlphaNumeric Format 
                         (Accepted upto 5 characters).</li>
                     <li style={{ marginTop:'15px', lineHeight:'24px' }}>
-                        If you adding course Data, nake sure Course Id is empty (Backend will generate Course Id).</li>
+                        If you adding course Data, make sure Course Id given by you in AlphaNumeric Format 
+                        (Accepted upto 5 characters).</li>
                 </ul>
             </div>
             <div align="center">
@@ -85,8 +98,22 @@ const UploadData = () =>{
     </Row>
     <Row>
       <Col md={12}>
-        <div className="mtop15p"><HeaderTitle title="File Uploaded Logs" /></div>
-        <div align="center" className="mtop15p" style={{ color:'#555', fontSize:'13px' }}><i>No Files were found to display here</i></div>
+        <div style={{ marginBottom:'45px' }}>
+          <div className="mtop15p"><HeaderTitle title="File Uploaded Logs" /></div>
+          {uploadedList?.length>0?(<Row>
+            {uploadedList?.map((file,index)=>{
+              return (<Col key={index} md={4}>
+                <div align="center" className="ud-file-box">
+                  <div className="ud-file-icon"><Icon type="FontAwesome" name="fa-file-excel-o" size={52} /></div>
+                  <div className="mtop15p ud-file-label"><b>{file}</b></div>
+                </div>
+              </Col>)
+            })}
+          </Row>):(<div align="center" className="mtop15p ud-file-placeholder">
+            <i>No Files were found to display here</i> {uploadedList?.length}
+          </div>)}
+          
+        </div>
       </Col>
     </Row>
   </ContainerFluid>
