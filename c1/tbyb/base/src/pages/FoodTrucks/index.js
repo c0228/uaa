e86a0342cce1fb7c9b90cfc07d389958e68eed7d
n0228 <nlnrao228@gmail.com>
2024-10-02 19:31:43 +0530

    Updated Projects

--- c1/tbyb/base/src/pages/FoodTrucks/index.js
+++ c1/tbyb/base/src/pages/FoodTrucks/index.js
@@ -7,85 +7,85 @@ import Footer from '@Templates/Footer/index.js';
 const FoodTrucks = () =>{
  useEffect(()=>{ document.body.style.backgroundColor='#ffeded'; },[]);
  const data = [{
-    "date":"Oct 3",
+    "date":"Oct 03",
     "day":"Thursday",
-    "time":{"from":"6 PM", "to":"10 PM" },
+    "time":{"from":"06 PM", "to":"10 PM" },
     "truck": "Capones Chicago Eats",
  },{
-   "date":"Oct 4",
+   "date":"Oct 04",
    "day":"Friday",
-   "time":{"from":"5 PM", "to":"10 PM" },
+   "time":{"from":"05 PM", "to":"10 PM" },
    "truck": "Getting Sauced in the City",
 },{
-   "date":"Oct 5",
+   "date":"Oct 05",
    "day":"Saturday",
-   "time":{"from":"6 PM", "to":"10 PM" },
+   "time":{"from":"06 PM", "to":"10 PM" },
    "truck": "Taqueria Dominguez",
 },{
-   "date":"Oct 6",
+   "date":"Oct 06",
    "day":"Sunday",
-   "time":{"from":"2 PM", "to":"9 PM" },
+   "time":{"from":"02 PM", "to":"09 PM" },
    "truck": "Capones Chicago Eats",
 },{
    "date":"Oct 10",
    "day":"Sunday",
-   "time":{"from":"2 PM", "to":"9 PM" },
+   "time":{"from":"02 PM", "to":"09 PM" },
    "truck": "Capones Chicago Eats",
 },{
    "date":"Oct 11",
    "day":"Friday",
-   "time":{"from":"5 PM", "to":"10 PM" },
+   "time":{"from":"05 PM", "to":"10 PM" },
    "truck": "Tacos Mama Chavez",
 },{
    "date":"Oct 12",
    "day":"Saturday",
-   "time":{"from":"5 PM", "to":"10 PM" },
+   "time":{"from":"05 PM", "to":"10 PM" },
    "truck": "Shawarma Bowl",
 },{
    "date":"Oct 13",
    "day":"Sunday",
-   "time":{"from":"2 PM", "to":"9 PM" },
+   "time":{"from":"02 PM", "to":"09 PM" },
    "truck": "Capones Chicago Eats",
 }];
 const data2 =[{
    "date":"Oct 17",
    "day":"Thursday",
-   "time":{"from":"6 PM", "to":"10 PM" },
+   "time":{"from":"06 PM", "to":"10 PM" },
    "truck": "Capones Chicago Eats",
 },{
    "date":"Oct 18",
    "day":"Friday",
-   "time":{"from":"6 PM", "to":"10 PM" },
+   "time":{"from":"06 PM", "to":"10 PM" },
    "truck": "Atlas Nomads",
 },{
    "date":"Oct 19",
    "day":"Saturday",
-   "time":{"from":"6 PM", "to":"10 PM" },
+   "time":{"from":"06 PM", "to":"10 PM" },
    "truck": "The Tasty Turkey Que",
 },{
    "date":"Oct 20",
    "day":"Sunday",
-   "time":{"from":"2 PM", "to":"9 PM" },
+   "time":{"from":"02 PM", "to":"09 PM" },
    "truck": "Capones Chicago Eats",
 },{
    "date":"Oct 24",
    "day":"Thursday",
-   "time":{"from":"6 PM", "to":"10 PM" },
+   "time":{"from":"06 PM", "to":"10 PM" },
    "truck": "Capones Chicago Eats",
 },{
    "date":"Oct 25",
    "day":"Friday",
-   "time":{"from":"6 PM", "to":"10 PM" },
+   "time":{"from":"06 PM", "to":"10 PM" },
    "truck": "Tacos Mama Chavaz",
 },{
    "date":"Oct 26",
    "day":"Saturday",
-   "time":{"from":"6 PM", "to":"10 PM" },
+   "time":{"from":"06 PM", "to":"10 PM" },
    "truck": "El Rey Del Taco",
 },{
    "date":"Oct 27",
    "day":"Sunday",
-   "time":{"from":"2 PM", "to":"9 PM" },
+   "time":{"from":"02 PM", "to":"09 PM" },
    "truck": "Capones Chicago Eats",
 }];
  return (<div>
@@ -105,6 +105,24 @@ const data2 =[{
                   <div align="center" className="heading-title" style={{ marginTop:'15px' }}><b>OCTOBER FOOD TRUCKS</b></div>
                   </Col>
                </Row>
+               <div className="mobile-view">
+               <table style={{ width:'100%' }}>
+                <tbody>
+                    {[...data,...data2]?.map((d,i)=>{
+                        return (<tr key={i}>
+                         <td style={{ padding:'15px' }}>
+                            <div style={{ letterSpacing:'0.2px' }}><b>{d?.day}</b></div>
+                            <div><b>({d?.time?.from} - {d?.time?.to})</b></div>
+                         </td>
+                         <td style={{ fontSize:'16px', color:'#bb0505', padding:'15px', borderRight:'1px solid #ccc' }}><div><b>{d?.date}</b></div></td>
+                         <td><div style={{ padding:'15px', textTransform:'uppercase' }}><b>{d?.truck}</b></div></td>
+                        </tr>);
+                    })}
+                    
+                </tbody>
+            </table>
+               </div>
+               <div className="desktop-view">
                <Row>
                <Col md={6}>
             <table style={{ width:'100%' }}>
@@ -142,6 +160,7 @@ const data2 =[{
                {/*<img src={process.env.PROJECT_URL+"assets/food-truck/2.png"} style={{ width:'90%', height:'auto' }} />*/}
             </Col>
                </Row>
+               </div>
             </Col>
         </Row>
     </ContainerFluid>
