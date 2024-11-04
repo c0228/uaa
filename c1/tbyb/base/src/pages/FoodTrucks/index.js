f649347d1cfced4f315090470238266b1f882891
n0228 <nlnrao228@gmail.com>
2024-11-04 21:40:53 +0530

    Updated Projects

--- c1/tbyb/base/src/pages/FoodTrucks/index.js
+++ c1/tbyb/base/src/pages/FoodTrucks/index.js
@@ -6,88 +6,25 @@ import Footer from '@Templates/Footer/index.js';
 
 const FoodTrucks = () =>{
  useEffect(()=>{ document.body.style.backgroundColor='#ffeded'; },[]);
- const data = [{
-    "date":"Oct 03",
-    "day":"Thursday",
-    "time":{"from":"06 PM", "to":"10 PM" },
-    "truck": "Capones Chicago Eats",
- },{
-   "date":"Oct 04",
-   "day":"Friday",
-   "time":{"from":"05 PM", "to":"10 PM" },
-   "truck": "Getting Sauced in the City",
-},{
-   "date":"Oct 05",
-   "day":"Saturday",
-   "time":{"from":"06 PM", "to":"10 PM" },
-   "truck": "Taqueria Dominguez",
-},{
-   "date":"Oct 06",
-   "day":"Sunday",
-   "time":{"from":"02 PM", "to":"09 PM" },
-   "truck": "Capones Chicago Eats",
-},{
-   "date":"Oct 10",
-   "day":"Sunday",
-   "time":{"from":"02 PM", "to":"09 PM" },
-   "truck": "Capones Chicago Eats",
-},{
-   "date":"Oct 11",
-   "day":"Friday",
-   "time":{"from":"05 PM", "to":"10 PM" },
-   "truck": "Tacos Mama Chavez",
-},{
-   "date":"Oct 12",
-   "day":"Saturday",
-   "time":{"from":"05 PM", "to":"10 PM" },
-   "truck": "Shawarma Bowl",
-},{
-   "date":"Oct 13",
-   "day":"Sunday",
-   "time":{"from":"02 PM", "to":"09 PM" },
-   "truck": "Capones Chicago Eats",
-}];
-const data2 =[{
-   "date":"Oct 17",
-   "day":"Thursday",
-   "time":{"from":"06 PM", "to":"10 PM" },
-   "truck": "Capones Chicago Eats",
-},{
-   "date":"Oct 18",
-   "day":"Friday",
-   "time":{"from":"06 PM", "to":"10 PM" },
-   "truck": "Atlas Nomads",
-},{
-   "date":"Oct 19",
-   "day":"Saturday",
-   "time":{"from":"06 PM", "to":"10 PM" },
-   "truck": "The Tasty Turkey Que",
-},{
-   "date":"Oct 20",
-   "day":"Sunday",
-   "time":{"from":"02 PM", "to":"09 PM" },
-   "truck": "Capones Chicago Eats",
-},{
-   "date":"Oct 24",
-   "day":"Thursday",
-   "time":{"from":"06 PM", "to":"10 PM" },
-   "truck": "Capones Chicago Eats",
-},{
-   "date":"Oct 25",
-   "day":"Friday",
-   "time":{"from":"06 PM", "to":"10 PM" },
-   "truck": "Tacos Mama Chavaz",
-},{
-   "date":"Oct 26",
-   "day":"Saturday",
-   "time":{"from":"06 PM", "to":"10 PM" },
-   "truck": "El Rey Del Taco",
-},{
-   "date":"Oct 27",
-   "day":"Sunday",
-   "time":{"from":"02 PM", "to":"09 PM" },
-   "truck": "Capones Chicago Eats",
-}];
+ const data = [{ "date":"Nov 01", "day":"Friday", "time":{ "from":"06 PM", "to":"10 PM" }, "truck": "Cousins Maine Lobster" },
+         { "date":"Nov 02", "day":"Saturday", "time":{ "from":"05 PM", "to":"10 PM" }, "truck": "Truman's Smokehouse" },
+         { "date":"Nov 03", "day":"Sunday", "time":{"from":"02 PM", "to":"07 PM" }, "truck": "Kings Breakfast and more" },
+         { "date": "Nov 05 - 07", "day":"Tuesday - Thursday", "time":{"from":"05 PM", "to":"09 PM" }, "truck": "Capones Chicago Eats" },
+         { "date":"Nov 08", "day":"Friday", "time":{"from":"05 PM", "to":"10 PM" }, "truck": "St Vincent Spanish Cuisine" },
+         { "date":"Nov 09", "day":"Saturday", "time":{"from":"05 PM", "to":"10 PM" }, "truck": "Eat Thai Raleigh" },
+         { "date":"Nov 10", "day":"Sunday", "time":{"from":"05 PM", "to":"10 PM" }, "truck": "Capones Chicago Eats" },
+         { "date":"Nov 12 - 14", "day":"Tuesday - Thursday", "time":{"from":"05 PM", "to":"09 PM" }, "truck": "Capones Chicago Eats" },
+         { "date":"Nov 15", "day":"Friday", "time":{"from":"05 PM", "to":"10 PM" }, "truck": "Will & Pop" }];
+
+const data2 =[{ "date":"Nov 16", "day":"Saturday", "time":{"from":"05 PM", "to":"10 PM" }, "truck": "Shmack N plates Bistro" },
+         { "date":"Nov 17", "day":"Sunday", "time":{"from":"05 PM", "to":"10 PM" }, "truck": "Capones Chicago Eats" },
+         { "date":"Nov 19 - 21", "day":"Tuesday - Thursday", "time":{"from":"05 PM", "to":"09 PM" }, "truck": "Capones Chicago Eats" },
+         { "date":"Nov 22", "day":"Friday", "time":{"from":"05 PM", "to":"10 PM" }, "truck": "Captain Gastro" },
+         { "date":"Nov 23", "day":"Saturday", "time":{"from":"05 PM", "to":"10 PM" }, "truck": "Mr. Cheesesteak" },
+         { "date":"Nov 24", "day":"Sunday", "time":{"from":"02 PM", "to":"09 PM" }, "truck": "Capones Chicago Eats" },
+         { "date":"Nov 26 - 28", "day":"Tuesday - Thursday", "time":{"from":"05 PM", "to":"09 PM" }, "truck": "Capones Chicago Eats" },
+         { "date":"Nov 29", "day":"Friday", "time":{"from":"05 PM", "to":"10 PM" }, "truck": "Bruno Burgers" },
+         { "date":"Nov 30", "day":"Saturday", "time":{"from":"05 PM", "to":"10 PM" }, "truck": "Kings Breakast and more" }];
  return (<div>
     <div style={{ position:'absolute', top:'40px' }}>
         <img src={"./assets/images/logo.png"} style={{padding:'5px', width:'100px', height:'auto' }} />
@@ -96,13 +33,10 @@ const data2 =[{
     <div style={{ marginBottom:'35px' }}>
     <ContainerFluid>
         <Row>
-            <Col md={4}>
-               <img src={process.env.PROJECT_URL+"assets/food-truck/1.png"} style={{ marginTop:'75px', width:'90%', height:'auto' }} />
-            </Col>
-            <Col md={8}>
+            <Col md={9}>
                <Row>
                   <Col md={12}>
-                  <div align="center" className="heading-title" style={{ marginTop:'15px' }}><b>OCTOBER FOOD TRUCKS</b></div>
+                  <div align="center" className="heading-title" style={{ marginTop:'15px' }}><b>NOVEMBER FOOD TRUCKS</b></div>
                   </Col>
                </Row>
                <div className="mobile-view">
@@ -161,6 +95,10 @@ const data2 =[{
             </Col>
                </Row>
                </div>
+            </Col>
+            <Col md={3}>
+               <img src={process.env.PROJECT_URL+"assets/food-truck/1.png"} style={{ marginTop:'75px', width:'90%', height:'auto' }} />
+               <img src={process.env.PROJECT_URL+"assets/food-truck/2.png"} style={{ marginTop:'75px', width:'90%', height:'auto' }} />
             </Col>
         </Row>
     </ContainerFluid>
