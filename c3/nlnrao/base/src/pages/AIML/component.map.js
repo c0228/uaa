e01f23cb5cd8fd1b239f11b41fd013ad0c30ad61
N.L.N.Rao <nlnrao228@gmail.com>
2025-03-23 16:36:36 +0530

    Updated Projects

--- c3/nlnrao/base/src/pages/AIML/component.map.js
+++ c3/nlnrao/base/src/pages/AIML/component.map.js
@@ -1,6 +1,6 @@
-import MLTechniques from "./content/01-types-of-ml-techniques/index.js";
-import TF3DObjectDetectionPipeline from "./content/02-tf-3D-object-detection-pipeline/index.js";
-import TFNewCategoryObjectDetection from "./content/03-tf-new-category-object-detection/index.js";
+import MLTechniques from "./content/ML/01-types-of-ml-techniques/index.js";
+import TF3DObjectDetectionPipeline from "./content/TensorFlow/01-tf-3D-object-detection-pipeline/index.js";
+import TFNewCategoryObjectDetection from "./content/TensorFlow/02-tf-new-category-object-detection/index.js";
 
 export const AIMLComponentMap = {
  "MLTechniques": MLTechniques,
