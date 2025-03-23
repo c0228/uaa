4adb9a8ac322d9aba585180941a2afecfbe145a5
N.L.N.Rao <nlnrao228@gmail.com>
2025-03-23 14:56:51 +0530

    Updated Projects

--- c3/nlnrao/base/src/templates/Footer/index.js
+++ c3/nlnrao/base/src/templates/Footer/index.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { Row, Col } from "e-ui-react";
+import './index.css';
 
 const Footer = () =>{
- return (<div style={{ marginBottom:'15px' }}>
-    &copy; 2023
- </div>);
+ return (<div className="app-blog-footer">
+    <Row>
+       <Col md={12}>
+       <div align="center" style={{ fontSize:'11px' }}>
+          Copyright &#169; 2021 - 2024 | All Rights Reserved | nellutlalnrao.com
+       </div>
+       </Col>
+    </Row>
+  </div>);
 };
 
 export default Footer;
