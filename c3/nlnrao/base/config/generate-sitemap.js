10989c4a20475e2cb989802a7e4d9919c46e4d57
Nellutla L N Rao <nlnrao228@gmail.com>
2025-06-23 15:32:33 +0530

    Updated projects

--- c3/nlnrao/base/config/generate-sitemap.js
+++ c3/nlnrao/base/config/generate-sitemap.js
@@ -3,7 +3,7 @@ import path from "path";
 import { fileURLToPath } from 'url';
 import dotenv from "dotenv";
 
-dotenv.config({ path: ".env.production" });
+dotenv.config({ path: "env/main/.env.production" });
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
