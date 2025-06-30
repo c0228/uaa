3236a65b8b7faf9e2b474bde5cd397c8a813fc9c
Nellutla L N Rao <nlnrao228@gmail.com>
2025-06-30 17:05:08 +0530

    Updated projects

--- c3/nlnrao/base/src/main/pages/DSA/system-design/lossy-count/components/conclusion/index.js
+++ c3/nlnrao/base/src/main/pages/DSA/system-design/lossy-count/components/conclusion/index.js
@@ -3,6 +3,23 @@ import { Card } from "e-ui-react";
 
 const Conclusion = () =>{
  return (<div className="mtop15p lh28p fs16p">
+    <div className="mtop15p"><h2 className="blog-head"><b>Key Benefits:</b></h2></div>
+    <div>
+      <ul>
+         <li className="mtop5p">Low memory footprint</li>
+         <li className="mtop5p">Supports real-time analytics</li>
+         <li className="mtop5p">Provides bounded inaccuracy (controlled by <code><b>&epsilon;</b></code>)</li>
+      </ul>
+    </div>
+    <div className="mtop15p"><h2 className="blog-head"><b>Use Cases:</b></h2></div>
+    <div>
+      <ul>
+         <li className="mtop5p">Top search terms on a website</li>
+         <li className="mtop5p">Trending topics in social media</li>
+         <li className="mtop5p">Frequent IPs in network traffic</li>
+      </ul>
+    </div>
+
     <div className="mtop15p"><h2 className="blog-head"><b>Conclusion</b></h2></div>
     <div className="mtop15p">The <b>Lossy Count algorithm</b> is an excellent tool 
     for approximate <b>frequency counting in streaming environments</b>. It’s efficient, simple, and 
@@ -14,22 +31,7 @@ const Conclusion = () =>{
          <li className="mtop5p">Real-time analytics requirements</li>
       </ul>
     </div>
-    <div className="mtop15p"><h4 className="blog-head"><b>Key Benefits:</b></h4></div>
-    <div>
-      <ul>
-         <li className="mtop5p">Low memory footprint</li>
-         <li className="mtop5p">Supports real-time analytics</li>
-         <li className="mtop5p">Provides bounded inaccuracy (controlled by <code><b>&epsilon;</b></code>)</li>
-      </ul>
-    </div>
-    <div className="mtop15p"><h4 className="blog-head"><b>Use Cases:</b></h4></div>
-    <div>
-      <ul>
-         <li className="mtop5p">Top search terms on a website</li>
-         <li className="mtop5p">Trending topics in social media</li>
-         <li className="mtop5p">Frequent IPs in network traffic</li>
-      </ul>
-    </div>
+   
     <div className="mtop5p">
       <Card padding={15} backgroundColor="#eee">
          If you're building systems for <b>streaming data processing</b>, <b>monitoring</b>, or <b>ranking</b>, 
