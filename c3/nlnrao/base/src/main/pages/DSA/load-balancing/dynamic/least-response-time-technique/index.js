20749606a14cbe024aaf8837781b95dfe20ecf67
Nellutla L N Rao <nlnrao228@gmail.com>
2025-07-02 14:39:18 +0530

    Updated projects

--- c3/nlnrao/base/src/main/pages/DSA/load-balancing/dynamic/least-response-time-technique/index.js
+++ c3/nlnrao/base/src/main/pages/DSA/load-balancing/dynamic/least-response-time-technique/index.js
@@ -3,12 +3,17 @@ import { Blog, Content, RightSide } from '@MainPages/DSA/components/blog-templat
 import LoadBalancingAlgorithms from "@MainPages/DSA/components/home/load-balance-algo/index.js";
 import DSANotations from '@MainPages/DSA/components/home/dsa-notations/index.js';
 import CacheEvictionStrategies from "@MainPages/DSA/components/home/cache-eviction-strategies/index.js";
+import Introduction from "./components/introduction/index.js";
+import Example from "./components/example/index.js";
+import Conclusion from "./components/conclusion/index.js";
 
 const LeastResponseTimeTechnique = ({ meta }) =>{
  return (<div>
   <Blog metaData={meta}>
     <Content>
-        Content
+      <Introduction />
+      <Example />
+      <Conclusion />
     </Content>
     <RightSide>
         <LoadBalancingAlgorithms />
---
+++ c3/nlnrao/base/src/main/pages/DSA/load-balancing/static/least-connections-technique/components/example/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Example = () =>{
+ return (<div className="mtop15p lh28p fs16p">
+    <div className="mtop15p"><h2 className="blog-head"><b>Example</b></h2></div>
+ </div>);
+};
+
+export default Example;
