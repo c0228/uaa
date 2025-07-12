811331a6aa267e2adfafc6967b88af78d392258b
Nellutla L N Rao <nlnrao228@gmail.com>
2025-07-12 10:35:01 +0530

    Updated Projects

---
+++ c3/nlnrao/base/src/main/pages/Web3/terminologies/modules/erc/components/Introduction/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import SimpleTable from "@MainComponents/simple-table/index.js";
+
+const Introduction = () =>{
+ return (<div className="mtop15p lh28p fs16p">
+    <div className="mtop15p"><h2 className="blog-head"><b>Introduction: What Is Cross-chain Technology?</b></h2></div>
+ </div>);
+};
+
+export default Introduction;
