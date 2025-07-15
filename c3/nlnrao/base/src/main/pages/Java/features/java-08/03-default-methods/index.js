c67c9e18935b55feccb848140376dbaf46e34f81
Nellutla L N Rao <nlnrao228@gmail.com>
2025-07-15 09:33:42 +0530

    Updated projects

--- c3/nlnrao/base/src/main/pages/Java/features/java-08/03-default-methods/index.js
+++ c3/nlnrao/base/src/main/pages/Java/features/java-08/03-default-methods/index.js
@@ -5,6 +5,9 @@ import { HeaderMenu } from '@MainConfig/HeaderMenu.js';
 import BlogArticleHeader from '@MainComponents/blog-article-header/index.js';
 import Terminologies from '@MainComponents/terminologies-list/index.js';
 import Routes from "@MainPages/Java/component.routes.json";
+import Introduction from "./components/Introduction/index.js";
+import Example from "./components/Example/index.js";
+import Conclusion from "./components/Conclusion/index.js";
 
 const DefaultMethods = ({ meta }) =>{
  return (<div className="mbot15p">
@@ -13,6 +16,9 @@ const DefaultMethods = ({ meta }) =>{
       <Row>
         <Col md={8}>
           <BlogArticleHeader metaData={meta} />
+          <Introduction />
+          <Example />
+          <Conclusion />
         </Col>
         <Col md={4}>
             <Terminologies title="Java 08 Features" isIndex={true} prefix="tech/java-08-features/" data={Routes?.urls} />
