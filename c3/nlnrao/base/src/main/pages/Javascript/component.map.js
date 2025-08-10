aea8ed6b6db391cb199f7a77317678104afa401d
Nellutla L N Rao <nlnrao228@gmail.com>
2025-08-10 13:23:04 +0530

    Updated projects

--- c3/nlnrao/base/src/main/pages/Javascript/component.map.js
+++ c3/nlnrao/base/src/main/pages/Javascript/component.map.js
@@ -4,15 +4,23 @@ import JSFunctionWritingWays from "@MainPages/Javascript/content/03-ways-of-writ
 
 import ArrowFunctions from "@MainPages/Javascript/terminologies/arrow-functions/index.js";
 import CallbackFunctions from "@MainPages/Javascript/terminologies/callback-functions/index.js";
+import CloneAnObject from "@MainPages/Javascript/terminologies/clone-an-object/index.js";
 import Closures from "@MainPages/Javascript/terminologies/closures/index.js";
 import ConceptOfNaN from "@MainPages/Javascript/terminologies/concept-of-nan/index.js";
+import Currying from "@MainPages/Javascript/terminologies/currying/index.js";
+import DebouncingVsThrottling from "@MainPages/Javascript/terminologies/debouncing-vs-throttling/index.js";
 import FuncDeclareFuncExpressions from "@MainPages/Javascript/terminologies/func-declarations-vs-func-expressions/index.js";
 import HigherOrderFunctions from "@MainPages/Javascript/terminologies/higher-order-functions/index.js";
 import JSHoisting from "@MainPages/Javascript/terminologies/hoisting/index.js";
 import IIFE from "@MainPages/Javascript/terminologies/iife/index.js";
+import InVshasOwnProperty from "@MainPages/Javascript/terminologies/in-vs-hasOwnProperty/index.js";
 import LexicalScope from "@MainPages/Javascript/terminologies/lexical-scope/index.js";
+import Memoization from "@MainPages/Javascript/terminologies/memoization/index.js";
 import NullUndefined from "@MainPages/Javascript/terminologies/null-vs-undefined/index.js";
+import ObjectArrayDestructuring from "@MainPages/Javascript/terminologies/object-destructuring-vs-array-destructuring/index.js";
+import ObjectFreezeSeal from "@MainPages/Javascript/terminologies/object-freeze-vs-object-seal/index.js";
 import PrimitiveReferences from "@MainPages/Javascript/terminologies/primitive-references/index.js";
+import ShallowDeepCopy from "@MainPages/Javascript/terminologies/shallowcopy-vs-deepcopy/index.js";
 import TemplateLiterals from "@MainPages/Javascript/terminologies/template-literals/index.js";
 import TruthyFalsy from "@MainPages/Javascript/terminologies/truthy-falsy/index.js";
 import TwoThreeEquals from "@MainPages/Javascript/terminologies/two-three-equals/index.js";
@@ -26,15 +34,23 @@ export const JSComponentMap = {
 
  "ArrowFunctions": ArrowFunctions,
  "CallbackFunctions": CallbackFunctions,
+ "CloneAnObject": CloneAnObject,
  "Closures": Closures,
  "ConceptOfNaN": ConceptOfNaN,
+ "Currying": Currying,
+ "DebouncingVsThrottling": DebouncingVsThrottling,
  "FuncDeclareFuncExpressions": FuncDeclareFuncExpressions,
  "HigherOrderFunctions": HigherOrderFunctions,
  "JSHoisting": JSHoisting,
  "IIFE": IIFE,
+ "InVshasOwnProperty": InVshasOwnProperty,
  "LexicalScope": LexicalScope,
+ "Memoization": Memoization,
  "NullUndefined": NullUndefined,
+ "ObjectArrayDestructuring": ObjectArrayDestructuring,
+ "ObjectFreezeSeal": ObjectFreezeSeal,
  "PrimitiveReferences": PrimitiveReferences,
+ "ShallowDeepCopy": ShallowDeepCopy,
  "TemplateLiterals": TemplateLiterals,
  "TruthyFalsy": TruthyFalsy,
  "TwoThreeEquals": TwoThreeEquals,
