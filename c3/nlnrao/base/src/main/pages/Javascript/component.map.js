7fd7e6235e0d948ab5866550041c9730bcde4750
Nellutla L N Rao <nlnrao228@gmail.com>
2025-08-10 21:24:46 +0530

    Updated Projects

--- c3/nlnrao/base/src/main/pages/Javascript/component.map.js
+++ c3/nlnrao/base/src/main/pages/Javascript/component.map.js
@@ -4,29 +4,48 @@ import JSFunctionWritingWays from "@MainPages/Javascript/content/03-ways-of-writ
 
 import ArrayPrototypeForEachVsMap from "@MainPages/Javascript/terminologies/array-prototype-foreach-vs-map/index.js";
 import ArrowFunctions from "@MainPages/Javascript/terminologies/arrow-functions/index.js";
+import BindCallApply from "@MainPages/Javascript/terminologies/bind-call-apply/index.js";
 import CallbackFunctions from "@MainPages/Javascript/terminologies/callback-functions/index.js";
 import CloneAnObject from "@MainPages/Javascript/terminologies/clone-an-object/index.js";
 import Closures from "@MainPages/Javascript/terminologies/closures/index.js";
 import ConceptOfNaN from "@MainPages/Javascript/terminologies/concept-of-nan/index.js";
 import Currying from "@MainPages/Javascript/terminologies/currying/index.js";
 import DebouncingVsThrottling from "@MainPages/Javascript/terminologies/debouncing-vs-throttling/index.js";
+import DOMVirtualDOMShadowDOM from "@MainPages/Javascript/terminologies/dom-virtualdom-shadowdom/index.js";
+import ES5VsES6 from "@MainPages/Javascript/terminologies/es5-vs-es6/index.js";
+import EventBubblingVsEventCapturing from "@MainPages/Javascript/terminologies/event-bubbling-vs-event-capturing/index.js";
+import EventDelegation from "@MainPages/Javascript/terminologies/event-delegation/index.js";
+import EventLoopVsCallStack from "@MainPages/Javascript/terminologies/eventloop-vs-callstack/index.js";
+import FetchVsAxios from "@MainPages/Javascript/terminologies/fetch-vs-axios/index.js";
 import FuncDeclareFuncExpressions from "@MainPages/Javascript/terminologies/func-declarations-vs-func-expressions/index.js";
 import HigherOrderFunctions from "@MainPages/Javascript/terminologies/higher-order-functions/index.js";
 import JSHoisting from "@MainPages/Javascript/terminologies/hoisting/index.js";
 import IIFE from "@MainPages/Javascript/terminologies/iife/index.js";
 import InVshasOwnProperty from "@MainPages/Javascript/terminologies/in-vs-hasOwnProperty/index.js";
+import InnerHTMLTextContent from "@MainPages/Javascript/terminologies/innerHTML-vs-innerText-vs-textContent/index.js";
+import IntersectionObserverAPI from "@MainPages/Javascript/terminologies/intersection-observer-api/index.js";
 import LexicalScope from "@MainPages/Javascript/terminologies/lexical-scope/index.js";
+import LocalStorageVsSessionStorageVsCookies from "@MainPages/Javascript/terminologies/localStorage-vs-sessionStorage-vs-cookies/index.js";
 import Memoization from "@MainPages/Javascript/terminologies/memoization/index.js";
 import NullUndefined from "@MainPages/Javascript/terminologies/null-vs-undefined/index.js";
 import ObjectArrayDestructuring from "@MainPages/Javascript/terminologies/object-destructuring-vs-array-destructuring/index.js";
 import ObjectFreezeSeal from "@MainPages/Javascript/terminologies/object-freeze-vs-object-seal/index.js";
+import PreventDefaultStopPropagation from "@MainPages/Javascript/terminologies/preventDefault-vs-stopPropagation/index.js";
 import PrimitiveReferences from "@MainPages/Javascript/terminologies/primitive-references/index.js";
+import Promises from "@MainPages/Javascript/terminologies/promises/index.js";
+import PrototypalInheritance from "@MainPages/Javascript/terminologies/prototypal-inheritance/index.js";
+import PureFunctionsVsImpureFunctions from "@MainPages/Javascript/terminologies/pure-functions-vs-impure-functions/index.js";
+import QuerySelectorVsGetElementById from "@MainPages/Javascript/terminologies/querySelector-vs-getElementById/index.js";
+import SetTimeoutVsSetImmediate from "@MainPages/Javascript/terminologies/setTimeout-vs-setImmediate/index.js";
 import ShallowDeepCopy from "@MainPages/Javascript/terminologies/shallowcopy-vs-deepcopy/index.js";
 import TemplateLiterals from "@MainPages/Javascript/terminologies/template-literals/index.js";
+import ThisKeyword from "@MainPages/Javascript/terminologies/this/index.js";
 import TruthyFalsy from "@MainPages/Javascript/terminologies/truthy-falsy/index.js";
 import TwoThreeEquals from "@MainPages/Javascript/terminologies/two-three-equals/index.js";
 import TypeCoercion from "@MainPages/Javascript/terminologies/type-coercion/index.js";
 import TypeofInstanceOf from "@MainPages/Javascript/terminologies/typeof-vs-instanceof/index.js";
+import WebWorkers from "@MainPages/Javascript/terminologies/web-workers/index.js";
+import WindowVsDocument from "@MainPages/Javascript/terminologies/window-vs-document/index.js"
 
 export const JSComponentMap = {
  "JSONArrayCrud": JSONArrayCrud,
@@ -35,27 +54,46 @@ export const JSComponentMap = {
 
  "ArrayPrototypeForEachVsMap": ArrayPrototypeForEachVsMap,
  "ArrowFunctions": ArrowFunctions,
+ "BindCallApply": BindCallApply,
  "CallbackFunctions": CallbackFunctions,
  "CloneAnObject": CloneAnObject,
  "Closures": Closures,
  "ConceptOfNaN": ConceptOfNaN,
  "Currying": Currying,
  "DebouncingVsThrottling": DebouncingVsThrottling,
+ "DOMVirtualDOMShadowDOM": DOMVirtualDOMShadowDOM,
+ "ES5VsES6": ES5VsES6,
+ "EventBubblingVsEventCapturing": EventBubblingVsEventCapturing,
+ "EventDelegation": EventDelegation,
+ "EventLoopVsCallStack": EventLoopVsCallStack,
+ "FetchVsAxios": FetchVsAxios,
  "FuncDeclareFuncExpressions": FuncDeclareFuncExpressions,
  "HigherOrderFunctions": HigherOrderFunctions,
  "JSHoisting": JSHoisting,
  "IIFE": IIFE,
  "InVshasOwnProperty": InVshasOwnProperty,
+ "InnerHTMLTextContent": InnerHTMLTextContent,
+ "IntersectionObserverAPI": IntersectionObserverAPI,
  "LexicalScope": LexicalScope,
+ "LocalStorageVsSessionStorageVsCookies": LocalStorageVsSessionStorageVsCookies,
  "Memoization": Memoization,
  "NullUndefined": NullUndefined,
  "ObjectArrayDestructuring": ObjectArrayDestructuring,
  "ObjectFreezeSeal": ObjectFreezeSeal,
+ "PreventDefaultStopPropagation": PreventDefaultStopPropagation,
  "PrimitiveReferences": PrimitiveReferences,
+ "Promises": Promises,
+ "PrototypalInheritance": PrototypalInheritance,
+ "PureFunctionsVsImpureFunctions": PureFunctionsVsImpureFunctions,
+ "QuerySelectorVsGetElementById": QuerySelectorVsGetElementById,
+ "SetTimeoutVsSetImmediate": SetTimeoutVsSetImmediate,
  "ShallowDeepCopy": ShallowDeepCopy,
  "TemplateLiterals": TemplateLiterals,
+ "ThisKeyword": ThisKeyword,
  "TruthyFalsy": TruthyFalsy,
  "TwoThreeEquals": TwoThreeEquals,
  "TypeCoercion": TypeCoercion,
- "TypeofInstanceOf": TypeofInstanceOf
+ "TypeofInstanceOf": TypeofInstanceOf,
+ "WebWorkers": WebWorkers,
+ "WindowVsDocument": WindowVsDocument
 };
---
+++ c3/nlnrao/base/src/main/pages/Javascript/terminologies/querySelector-vs-getElementById/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { ContainerFluid, Row, Col, Breadcrumb, Colors, Button, Select, Card, Switch } from "e-ui-react";
+import Header from '@MainTemplates/Header/index.js';
+import { HeaderMenu } from '@MainConfig/HeaderMenu.js';
+import BlogArticleHeader from '@MainComponents/blog-article-header/index.js';
+import Terminologies from '@MainComponents/terminologies-list/index.js';
+import JSRoutes from "@MainPages/Javascript/component.routes.json";
+
+const QuerySelectorVsGetElementById = ({ meta }) =>{
+ return (<div>
+  <Header menulinks={HeaderMenu} activeId="Home" />
+   <ContainerFluid>
+      <Row>
+        <Col md={8}>
+          <BlogArticleHeader metaData={meta} />
+        </Col>
+        <Col md={4}>
+            <Terminologies title="Javascript Terminologies" prefix="tech/javascript/terminologies/" data={JSRoutes?.urls} />
+        </Col>
+       </Row>
+    </ContainerFluid>
+ </div>);
+};
+
+export default QuerySelectorVsGetElementById;
