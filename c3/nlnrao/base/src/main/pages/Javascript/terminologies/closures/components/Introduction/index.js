bbb281e0f910438c5788511d35378ff5a0c9e8b9
Nellutla L N Rao <nlnrao228@gmail.com>
2025-08-12 19:29:19 +0530

    Updated projects

--- c3/nlnrao/base/src/main/pages/Javascript/terminologies/closures/components/Introduction/index.js
+++ c3/nlnrao/base/src/main/pages/Javascript/terminologies/closures/components/Introduction/index.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { Card } from "e-ui-react";
 
 const Introduction = () =>{
  return (<div className="mtop15p lh28p fs16p">
-    <div className="mtop15p"><h2 className="blog-head"><b>Introduction</b></h2></div>
+   <div className="mtop15p">
+      <Card padding={15} backgroundColor="#eee">
+         <div><h4 className="blog-head"><b>The Problem Before -</b></h4></div>
+      </Card>
+   </div>
+   <div className="mtop15p"><h2 className="blog-head"><b>Introduction</b></h2></div>
  </div>);
 };
 
