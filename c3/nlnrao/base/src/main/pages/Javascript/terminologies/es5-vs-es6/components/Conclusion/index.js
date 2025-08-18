3b2b7d5a4b30982c632a0f8efec79d9434939e9c
Nellutla L N Rao <nlnrao228@gmail.com>
2025-08-18 10:29:16 +0530

    Updated projects

--- c3/nlnrao/base/src/main/pages/Javascript/terminologies/es5-vs-es6/components/Conclusion/index.js
+++ c3/nlnrao/base/src/main/pages/Javascript/terminologies/es5-vs-es6/components/Conclusion/index.js
@@ -6,10 +6,10 @@ const Conclusion = () =>{
    <div className="mtop15p"><h2 className="blog-head"><b>Conclusion</b></h2></div>
    <div>
       <ul>
-         <li className="mtop5p">ES5 was a big step in making JavaScript consistent and widely usable.</li>
-         <li className="mtop5p">ES6 revolutionized the language, introducing features like let/const, 
+         <li className="mtop5p"><b>ES5</b> was a big step in making JavaScript consistent and widely usable.</li>
+         <li className="mtop5p"><b>ES6</b> revolutionized the language, introducing features like let/const, 
          arrow functions, classes, template literals, promises, modules, default parameters, and destructuring — making 
-         development more readable, maintainable, and scalable.</li>
+         development more <b>readable, maintainable, and scalable.</b></li>
          <li className="mtop5p">Today, developers rarely write pure ES5 directly. Instead, they use ES6+ features, 
          with transpilers like Babel ensuring older browsers can still run them.</li>
       </ul>
