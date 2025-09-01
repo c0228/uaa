7893d3e1a923ab62ff508f0d1c6477beffe07fa5
NLNRao <nlnrao228>
2025-09-01 21:24:52 +0530

    Updated Projects

--- c3/nlnrao/base/src/main/pages/AIML/terminologies/01-artifical-neuron/components/Example/index.js
+++ c3/nlnrao/base/src/main/pages/AIML/terminologies/01-artifical-neuron/components/Example/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Card } from "e-ui-react";
+import React, { useState } from "react";
+import { Card, Modal } from "e-ui-react";
 import { MathJax, MathJaxContext } from "better-react-mathjax";
 
 const Example = () =>{
@@ -20,11 +20,61 @@ const Example = () =>{
      </MathJaxContext>);
   };
   const GradientOneExample = () =>{
-     return (<MathJaxContext>
+    const [ showModel, setShowModal ] = useState(true);
+    const FormulaEvolution = ()=>{
+        return (<MathJaxContext>
+        <div style={{ fontSize:'14px' }}>
+            <div>
+                <Card padding={15} backgroundColor="#eee">
+                    <div><b>Differentiation Formulas:</b></div>
+                    <div>We know, implicity Differentiation Formula (in terms of y),</div>
+                    <div className="mtop5p">If <MathJax inline>{`\\( y = y(x) \\)`}</MathJax> and <b>c</b> is the constant value, then</div>
+                    <div>
+                       <b>(i)</b> <MathJax inline>{`\\( \\frac{d}{dx}(y^n) = n . y^{n-1} . \\frac{dy}{dx} \\)`}</MathJax>
+                    </div>
+                    <div className="mtop5p">
+                       <b>(ii)</b> <MathJax inline>{`\\( \\frac{d(c)}{dx} = 0 \\)`}</MathJax>
+                    </div>
+                    <div className="mtop5p">
+                       <b>(iii)</b> <MathJax inline>{`\\( \\frac{d(x)}{dx} = 1 \\)`}</MathJax>
+                    </div>
+                </Card>
+            </div>
+            <div className="mtop15p"><b>#1. Derving <MathJax inline>{`\\( \\frac{\\partial L}{\\partial \\omega} \\)`}</MathJax> Value:</b></div>
+            <div className="padLeft5p">
+                <div className="mtop5p"><b>Formula #1:</b> <MathJax inline>{`\\( L = ( y_{1} - y)^2 \\)`}</MathJax></div>
+                <div>Differentiating in terms of &omega;</div>
+                <div><MathJax inline>{`\\( \\implies \\frac{dL}{d\\omega} = 2 . (y_{1} - y). \\frac{dy_{1}}{d \\omega} - \\frac{dy}{d \\omega} \\)`}</MathJax></div>
+                <div>In above Statement, y is a Constant value. So, <MathJax inline>{`\\( \\frac{d(y)}{dx} = 0 \\)`}</MathJax></div>
+                <div>Therefore, it can be written as - </div>
+                <div><MathJax inline>{`\\( \\implies \\frac{dL}{d\\omega} = 2 . (y_{1} - y). \\frac{dy_{1}}{d \\omega} \\)`}</MathJax> <b>---&gt; EQ(1)</b></div> 
+            
+                <div className="mtop5p"><b>Formula #2:</b> <MathJax inline>{`\\( y_{1} = \\omega . x + b \\)`}</MathJax></div>
+                <div>Differentiating in terms of &omega;</div>
+                <div><MathJax inline>{`\\( \\implies \\frac{dy_{1}}{d\\omega} = \\frac{d\\omega}{d\\omega} .x + \\frac{db}{d\\omega} \\)`}</MathJax></div> 
+                <div>In above Statement, b is a Constant value.</div>
+                <div><MathJax inline>{`\\( \\implies \\frac{dy_{1}}{d\\omega} =  1 . x + 0 \\)`}</MathJax></div> 
+                <div><MathJax inline>{`\\( \\implies \\frac{dy_{1}}{d\\omega} =  x \\)`}</MathJax> <b>---&gt; EQ(2)</b></div> 
+                <div>Impementing <b>EQ(2)</b> in <b>EQ(1)</b>, we will get -</div>
+                <Card padding={15}>
+                    <div><MathJax inline>{`\\( \\frac{dL}{d\\omega} =   2 . (y_{1} - y). x \\)`}</MathJax></div>
+                </Card>
+            </div> 
+            <div className="mtop15p"><b>#2. Derving <MathJax inline>{`\\( \\frac{\\partial L}{\\partial b} \\)`}</MathJax> Value:</b></div>
+        </div>
+        </MathJaxContext>);
+    };
+    return (<MathJaxContext>
        <div className="mathJaxContext-formula">
          <p>
            <MathJax inline>{`\\( \\frac{\\partial  L}{\\partial  \\omega} = 2 ( y_{1} - y ) x ,  \\frac{\\partial  L}{\\partial  b} = 2 ( y_{1} - y ) \\)`}</MathJax>
+           <span className="pull-right" style={{ cursor:'pointer', paddingRight:'15px', fontSize:'13px', color:'#0d6efd' }} 
+            onClick={()=>setShowModal(true)}><u>How?</u></span>
+           <Modal title="How this values got derived?" show={showModel} onClose={setShowModal}>
+             <FormulaEvolution />
+           </Modal>
          </p>
+         
        </div>
      </MathJaxContext>);
   };
@@ -70,7 +120,7 @@ const Example = () =>{
     <div className="mtop5p">This is <b>Mean Squared Error</b> (for one example, it’s just squared error).</div>
 
     <div className="mtop5p">
-        <Card padding={15}>
+        <Card padding={15} backgroundColor="#eee">
             <div><h4 className="blog-head"><b>Why do we take the square of the difference in the loss function? Why not 
                 just use the simple difference (prediction – actual)?</b></h4></div>
             <div className="mtop5p">If we define loss as:</div>
