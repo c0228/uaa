16b90404d6b1b7c5a10572dda1f5b01ab91673be
NLNRao <nlnrao228>
2026-04-27 21:42:39 +0530

    Updated Projects

---
+++ c3/nlnrao/base/src/main/pages/MWAPIDesign/content/06-g-rpc/components/Examples/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Card } from "e-ui-react";
+
+const Examples = () =>{
+ return (<div className="mtop15p mbot15p lh28p fs16p">
+    <div className="mtop15p"><h2 className="blog-head"><b>Examples</b></h2></div>
+ </div>);
+};
+
+export default Examples;
