a5b4eec067810c406f79b0bc14aa046fa822edcc
NLNRao <nlnrao228>
2026-05-05 16:22:57 +0530

    Updated projects

--- c3/nlnrao/base/src/showcase/pages/AIML/content/01-phimini3-lsmvec/components/Introduction/index.js
+++ c3/nlnrao/base/src/showcase/pages/AIML/content/01-phimini3-lsmvec/components/Introduction/index.js
@@ -12,17 +12,6 @@ const Introduction = () =>{
         <li className="mtop5p"><b>AI Model:</b> <code><b>Microsoft Phi-mini-3</b></code> (Used for reasoning and generation)</li>
         <li className="mtop5p"><b>Embedded Vector Search Layer:</b> <code><b>LSM-Vec</b></code> (Used to generate vector embeddings for semantic retrieval)</li>
     </ul>
-    <div>Using an <b>embedded Retrieval-Augmented Generation (RAG) architecture,</b> the system avoids loading massive 
-    data into the prompt. Instead, it:</div>
-    <ol>
-        <li className="mtop5p"><b>Chunks and embeds</b> large datasets</li>
-        <li className="mtop5p"><b>Stores embeddings locally</b> using an efficient, disk-friendly vector structure</li>
-        <li className="mtop5p"><b>Retrieves only the most relevant context</b> at query time</li>
-        <li className="mtop5p"><b>Feeds minimal, high-signal context</b> to the small model for reasoning and generation</li>
-    </ol>
-    <div>This enables <b>long-context simulation,</b> where a small-context model behaves as if it has access to massive 
-    external memory.</div>
-    
  </div>);
 };
 
