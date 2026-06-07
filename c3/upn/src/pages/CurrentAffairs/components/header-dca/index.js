c90405b74aad97b7db38a92c2ca6ee7ac5ed5fa2
NLNRao <nlnrao228>
2026-06-07 17:57:23 +0530

    Updated projects

--- c3/upn/src/pages/CurrentAffairs/components/header-dca/index.js
+++ c3/upn/src/pages/CurrentAffairs/components/header-dca/index.js
@@ -55,7 +55,6 @@ const HeaderDCA = ({ date, data }) =>{
         <Row>
             <Col md={8}>
                <div className="d-flex gap-2 mt-3">
-                     {searchDCAFormData?.date}
                      <TextBox name="searchCurrentAffairs" placeholder="Search Current Affairs" 
                            onChange={(data)=>searchDCAFormFieldHandler("text", data?.value)} />
                      <DateTimePicker type="datePicker" id="date" name="date" value={searchDCAFormData?.date} 
