08799877a6c863bca74c4e7b418229ecc0b7bb72
NLNRao <nlnrao228>
2026-06-11 09:36:59 +0530

    Updated Projects

--- c3/upn/stub/src/services/daily-current-affairs/SearchByCategories.js
+++ c3/upn/stub/src/services/daily-current-affairs/SearchByCategories.js
@@ -3,13 +3,18 @@ import SearchByCategoriesData from '#StaticData/data-dca-searchbycategories.json
 export const getArticlesByNiches = (req, res) => {
     try {
        const { category, subCategory, pageIndex } = req.body;
-       const data = structuredClone(SearchByCategoriesData);
+       const apiData = structuredClone(SearchByCategoriesData);
 
-        data.current.category = category;
-        data.current.subCategory = subCategory;
-        data.current.pageIndex = pageIndex;
+        apiData.current.category = category;
+        apiData.current.subCategory = subCategory;
+        apiData.current.pageIndex = pageIndex;
+        
+        console.log("apiData?.current?.data?.length: ", apiData?.current?.data?.length);
+        for(let i=0;i<apiData?.current?.data?.length;i++){
+            apiData.current.data[i].title = apiData?.current?.data[i].title+' Page - '+pageIndex;
+        }
 
-        res.json(data);
+        res.json(apiData);
     } catch (error) {
         res.status(500).json({
             status: 'FAILURE',
