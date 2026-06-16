60c9ef97c498fda94352f467f9a906ba70e16685
NLNRao <nlnrao228>
2026-06-16 20:09:23 +0530

    Updated projects

--- c3/upn/src/pages/NichesManager/components/ViewCategories/index.js
+++ c3/upn/src/pages/NichesManager/components/ViewCategories/index.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { ContainerFluid, Row, Col, Badge, Card } from "e-ui-react";
+import { ContainerFluid, Row, Col, Badge, Card, Icon, TextBox  } from "e-ui-react";
+import { AppColors } from "@Utils/AppColorManager.js";
 import { callAPI } from "@Services/ApiManager.js";
 
 const ViewCategories = () =>{
+ const [displayMode, setDisplayMode] = useState('view'); // view / edit
  const [apiResponseData, setApiResponseData] = useState();
  useEffect(()=>{
     fetch("http://localhost/projects/uaa/c3/upn/nexus/niches/category/view").then(response=>response.json())
@@ -27,15 +29,43 @@ const ViewCategories = () =>{
                     <div className="mbot15p">
                     <Card padding={15}>
                         <div style={{ minHeight:'510px' }}>
-                        <div><Badge type="primary-o" label={<b>{r?.["cat_id"]}</b>} size="12" style={{ marginRight:'5px' }} /></div>
+                        <div>
+                            <Badge type="primary-o" label={<b>{r?.["cat_id"]}</b>} size="12" style={{ marginRight:'5px' }} />
+                            <div className="pull-right" style={{ cursor:'pointer' }} onClick={()=>setDisplayMode('edit')}>
+                                <Icon type="FontAwesome" name="fa-edit" size={14} color={AppColors?.["primary"]?.["bg-dark"]} />
+                            </div>
+                        </div>
                         <Row>
                             <Col md={6}>
-                                <div className="mtop5p"><h5><b>{enTitle} [{enDesc?.length} /1000]</b></h5></div>
-                                <div className="mtop15p">{enDesc}</div>
+                                <div className="mtop5p">
+                                    <h5><b>
+                                        {displayMode==='view' && (<>{enTitle}</>)} 
+                                        {displayMode==='edit' && (<>
+                                        <TextBox name="surname" 
+                                            placeholder="Enter Category Name" value={enTitle} />
+                                        </>)} 
+                                        <span className="pull-right">[{enDesc?.length} /1000]</span>
+                                        </b></h5>
+                                </div>
+                                <div className="mtop15p">
+                                    {displayMode==='view' && (<>{enDesc}</>)} 
+                                    {displayMode==='edit' && (<>{enDesc}</>)}
+                                </div>
                             </Col>
                             <Col md={6}>
-                                <div className="mtop5p"><h5><b>{hiTitle} [{hiDesc?.length} /1000]</b></h5></div>
-                                <div className="mtop15p">{hiDesc}</div>
+                                <div className="mtop5p">
+                                    <h5><b>
+                                        {displayMode==='view' && (<>{hiTitle}</>)} 
+                                        {displayMode==='edit' && (<>
+                                        <TextBox name="surname" 
+                                            placeholder="Enter Category Name" value={hiTitle} />
+                                        </>)}
+                                        <span className="pull-right">[{hiDesc?.length} /1000]</span>
+                                        </b></h5></div>
+                                <div className="mtop15p">
+                                    {displayMode==='view' && (<>{hiDesc}</>)} 
+                                    {displayMode==='edit' && (<>{hiDesc}</>)}
+                                </div>
                             </Col>
                         </Row>
                         </div>
