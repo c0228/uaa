1f41107c583aded8c74900150acbfaf8a2e340a8
NLNRao <nlnrao228>
2026-06-17 22:03:03 +0530

    Updated Projects

--- c3/upn/src/pages/NichesManager/components/ViewCategories/index.js
+++ c3/upn/src/pages/NichesManager/components/ViewCategories/index.js
@@ -2,6 +2,62 @@ import React, { useState, useEffect } from "react";
 import { ContainerFluid, Row, Col, Badge, Card, Icon, TextBox, TextArea, Button } from "e-ui-react";
 import { AppColors } from "@Utils/AppColorManager.js";
 
+const DisplayEdit = ({ formData, setFormData }) =>{
+ const [initialFormData, setInitialFormData] = useState(formData);
+ const handleEdit = () =>{
+    console.log("formData: ", formData);
+ };
+ const handleReset = () =>{
+   setFormData(initialFormData);
+ };
+ return (<>
+    <Row>
+        <Col md={6}>
+            <div>
+                <div>
+                 <TextBox name="enCatName" placeholder="Enter Category Name" value={formData?.enTitle} 
+                    onChange={(data)=>{
+                        setFormData({ ...formData, "enTitle": data?.value });
+                    }} />
+                 </div>
+                <div><span className="pull-right"><b>[{formData?.enDesc?.length} / 1000]</b></span></div>
+                <div className="mtop5p">
+                    <TextArea name="enCatDesc" placeholder="Enter Category Desc" 
+                        lines={23} value={formData?.enDesc} onChange={(data)=>{
+                            setFormData({ ...formData, "enDesc": data?.value });
+                        }} />
+                </div>
+            </div>
+        </Col>
+        <Col md={6}>
+            <div>
+                <div>
+                    <TextBox name="hiCatName" placeholder="Enter Category Name" value={formData?.hiTitle} 
+                        onChange={(data)=>{
+                            setFormData({ ...formData, "hiTitle": data?.value });
+                        }} />
+                </div>
+                <div><span className="pull-right"><b>[{formData?.hiDesc?.length} / 1000]</b></span></div>
+                <div className="mtop5p">
+                    <TextArea name="hiCatDesc" placeholder="Enter Category Desc" 
+                        lines={23} value={formData?.hiDesc} onChange={(data)=>{
+                            setFormData({ ...formData, "hiDesc": data?.value });
+                        }} />
+                </div>
+            </div>
+        </Col>
+    </Row>
+    <Row>
+        <Col md={12}>
+            <div align="right" className="mtop5p">
+                <Button type="success" size={11} style={{ marginRight:'5px' }} onClick={()=>handleEdit()}><b>Save</b></Button>
+                <Button type="danger" size={11} onClick={()=>handleReset()}><b>Reset</b></Button>
+            </div>
+        </Col>
+    </Row>
+    </>);
+ };
+
 const ViewCategories = () =>{
  const [displayMode, setDisplayMode] = useState({ op: "view", id: "" });
  const [apiResponseData, setApiResponseData] = useState([]);
@@ -10,14 +66,6 @@ const ViewCategories = () =>{
     return (<div className="mtop5p">
         <div><b>{title}</b></div>
         <div className="mtop5p">{desc}</div>
-    </div>);
- };
- const DisplayEdit = ({ lang, id, title, desc }) =>{
-    return (<div>
-     <div><TextBox name={id+"-"+lang+"-catName"} placeholder="Enter Category Name" value={title} /></div>
-     <div><span className="pull-right"><b>[{desc?.length} / 1000]</b></span></div>
-     <div className="mtop5p">
-        <TextArea name={id+"-"+lang+"-catDesc"} placeholder="Enter Category Desc" lines={23} value={desc} /></div>
     </div>);
  };
  useEffect(() => {
@@ -46,25 +94,20 @@ const ViewCategories = () =>{
                 <div>
                     <Badge type="primary-o" label={<b>{catId}</b>} size="12" style={{ marginRight:'5px' }} />
                     <span className="pull-right" style={{ cursor:'pointer' }} 
-                        onClick={()=>setDisplayMode({ op: 'edit', id: catId })}>
+                        onClick={()=>{
+                            setDisplayMode({ op: 'edit', id: catId })
+                            setFormData({ 
+                                "enTitle": enTitle,
+                                "enDesc": enDesc,
+                                "hiTitle": hiTitle,
+                                "hiDesc": hiDesc
+                            })
+                        }}>
                         <Icon type="FontAwesome" name="fa-edit" size={18} color="#0d6efd" />
                     </span>
                 </div>
                 <div className="mtop5p">
-                {isEditing?(<>
-                <Row>
-                    <Col md={6}><DisplayEdit lang="en"  title={enTitle} desc={enDesc} /></Col>
-                    <Col md={6}><DisplayEdit lang="hi"  title={hiTitle} desc={hiDesc} /></Col>
-                </Row>
-                <Row>
-                    <Col md={12}>
-                        <div align="right" className="mtop5p">
-                            <Button type="success" size={11} style={{ marginRight:'5px' }}><b>Save</b></Button>
-                            <Button type="danger" size={11}><b>Reset</b></Button>
-                        </div>
-                    </Col>
-                </Row>
-                </>):(<div>
+                {isEditing?(<><DisplayEdit formData={formData} setFormData={setFormData} /></>):(<div>
                     <Row>
                         <Col md={6}><DisplayView title={enTitle} desc={enDesc} /></Col>
                         <Col md={6}><DisplayView title={hiTitle} desc={hiDesc} /></Col>
