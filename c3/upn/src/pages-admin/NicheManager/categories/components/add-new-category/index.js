3a82c80ce4c00e7618290e993fb3dc7e77137603
NLNRao <nlnrao228>
2026-06-22 11:46:22 +0530

    Updated Projects

--- c3/upn/src/pages-admin/NicheManager/categories/components/add-new-category/index.js
+++ c3/upn/src/pages-admin/NicheManager/categories/components/add-new-category/index.js
@@ -1,16 +1,36 @@
 import React, { useState } from "react";
-import { ContainerFluid, Row, Col, Modal, Icon, Button } from "e-ui-react";
+import { ContainerFluid, Row, Col, Card, Modal, Icon, TextBox, TextArea, Button } from "e-ui-react";
 
 const AddNewCategory = () =>{
  const [showModal, setShowModal] = useState(false);
+  const Form = ({ langId, lang }) =>{
+     return (<div>
+         <Card padding={15}>
+             <div style={{ padding:'8px', backgroundColor:'#ddd', }}><b>{lang} Version</b></div>
+             <div className="pad15p">
+                 <div>
+                     <TextBox name={"add_"+langId+"_categoryName"} label="Category Name" placeholder="Enter New Category" />
+                 </div>
+                 <div className="mtop15p">
+                     <TextArea name={"add_"+langId+"_categoryDesc"} lines={12} 
+                      label="Category Desc" placeholder="Enter Category Description" />
+                 </div>
+             </div>
+         </Card>
+     </div>);
+  };
+
  return (<div>
-  <Modal title="Add New Category" width="800px" show={showModal} onClose={setShowModal}>
+  <Modal title="Add New Category" width="90%" show={showModal} onClose={setShowModal}>
     <div className="mtop15p mbot15p">
      <ContainerFluid>
         <Row>
-            <Col md={12}>
-            Test
-            </Col>
+             {[{ id:'en', lang:'English' },
+              { id:'hi', lang:'Hindi' }]?.map((d,i)=>{
+                 return (<Col key={i} md={6}>
+                     <Form langId={d?.id} lang={d?.lang} />
+                 </Col>);
+             })}
         </Row>
      </ContainerFluid>
     </div>
