6120f5d6b1dfaf502593d8a8500dca9c54491b17
NLNRao <nlnrao228>
2026-07-01 15:45:02 +0530

    Updated projects

--- c3/upn/src/components/exam-eligibility-calculator/components/form-reservations/index.js
+++ c3/upn/src/components/exam-eligibility-calculator/components/form-reservations/index.js
@@ -6,12 +6,14 @@ const FormReservations = () =>{
     <div><h5><b>3. Reservation & Relaxations</b></h5><hr/></div>
     <ContainerFluid>
         <Row>
-            <Col md={6}>
+            <Col md={12}>
                 <div className="mt-3">
-                    <Switch type="radio" id="PwBD" name="PwBD" 
+                    <Switch type="radio" id="PwBD" name="PwBD"  layout="horizontal"
                         label="Are you a Person with Benchmark Disability (PwBD)?" 
                         value={[{ id:'Yes', label:"Yes", value:"Yes"},
-                                { id:'No', label:"No", value:"No" }]} />
+                                { id:'No', label:"No", value:"No" }]} 
+                        onChange={(value)=>console.log("Result: ", value)}        
+                        />
                 </div>
             </Col>
         </Row>
