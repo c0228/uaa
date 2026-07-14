0c4238622cb5848135b8a0d3972dcbe86c7f846d
NLNRao <nlnrao228>
2026-07-14 17:53:15 +0530

    Updated projects

--- c3/upn/src/components/exam-eligibility-calculator/components/step-01-personal-info/index.js
+++ c3/upn/src/components/exam-eligibility-calculator/components/step-01-personal-info/index.js
@@ -52,19 +52,59 @@ const PersonalInfoForm = {
             "Tibetan Refugee":{
                 "en": (<>A <b>Tibetan refugee</b> who came to India before <b>1 January 1962</b> with the intention of 
                     permanently settling in India.</>),
-                "hi": ""
+                "hi": "एक तिब्बती शरणार्थी जो 1 जनवरी 1962 से पहले भारत में स्थायी रूप से बसने के इरादे से भारत आया था।"
             },
             "Person of Indian Origin (PIO)":{
                 "en": (<>A <b>Person of Indian Origin (PIO)</b> who migrated from Pakistan, Myanmar (Burma), Sri Lanka, 
                     Kenya, Uganda, Tanzania, Zambia, Malawi, Zaire (now the Democratic Republic of the Congo), Ethiopia, or 
                     Vietnam with the intention of permanently settling in India. Candidates in categories 2–5 require a 
                     Certificate of Eligibility from the Government of India.</>),
-                "hi": ""
+                "hi": "भारतीय मूल का ऐसा व्यक्ति (PIO) जो पाकिस्तान, म्यांमार (बर्मा), श्रीलंका, केन्या, युगांडा, तंजानिया, जाम्बिया, मलावी, "+
+                    " ज़ैरे (अब डेमोक्रेटिक रिपब्लिक ऑफ़ कांगो), इथियोपिया या वियतनाम से भारत में स्थायी रूप से बसने के इरादे से आया हो। कैटेगरी 2-5 में "+
+                    "आने वाले उम्मीदवारों को भारत सरकार से पात्रता प्रमाण-पत्र (Certificate of Eligibility) की आवश्यकता होती है।"
             }
         }
     },
     "category":{
-
+        "id":"category",
+        "enLabel":"Your Category",
+        "hiLabel":"आपका वर्ग",
+        "enPlaceholder":"Select your Category",
+        "hiPlaceholder":"अपनी श्रेणी चुनें",
+        "enOptions":[{ id:'General (Unreserved / UR)', label:'General (Unreserved / UR)', value:'General (Unreserved / UR)' },
+                { id:'Economically Weaker Section (EWS)', label:'Economically Weaker Section (EWS)', value:'Economically Weaker Section (EWS)' },
+                { id:'Other Backward Class (OBC - Non-Creamy Layer)', label:'Other Backward Class (OBC - Non-Creamy Layer)', value:'Other Backward Class (OBC - Non-Creamy Layer)' },
+                { id:'Scheduled Caste (SC)', label:'Scheduled Caste (SC)', value:'Scheduled Caste (SC)' },
+                { id:'Scheduled Tribe (ST)', label:'Scheduled Tribe (ST)', value:'Scheduled Tribe (ST)' }],
+        "hiOptions":[{ id:'General (Unreserved / UR)', label:'सामान्य (अनारक्षित / यू.आर)', value:'General (Unreserved / UR)' },
+                { id:'Economically Weaker Section (EWS)', label:'आर्थिक रूप से कमजोर वर्ग (ई.डब्ल्यू.एस)', value:'Economically Weaker Section (EWS)' },
+                { id:'Other Backward Class (OBC - Non-Creamy Layer)', label:'अन्य पिछड़ा वर्ग (ओ.बी.सी - नॉन-क्रीमी लेयर)', value:'Other Backward Class (OBC - Non-Creamy Layer)' },
+                { id:'Scheduled Caste (SC)', label:'अनुसूचित जाति (एस.सी)', value:'Scheduled Caste (SC)' },
+                { id:'Scheduled Tribe (ST)', label:'अनुसूचित जनजाति (एस.टी)', value:'Scheduled Tribe (ST)' }],
+        "optionInfo":{
+            "General (Unreserved / UR)":{
+                "en": (<><b>General (Unreserved / UR) -</b> No reservation</>),
+                "hi":"सामान्य (अनारक्षित / यू.आर) - कोई आरक्षण नहीं"
+            },
+            "Economically Weaker Section (EWS)":{
+                "en": (<><b>Economically Weaker Sections (EWS) -</b> Reservation for economically weaker candidates who are not covered 
+                    under SC, ST, or OBC reservations.</>),
+                "hi": "आर्थिक रूप से कमजोर वर्ग (ई.डब्ल्यू.एस) - आर्थिक रूप से कमजोर उन उम्मीदवारों के लिए आरक्षण जो एस.सी, एस.टी या ओ.बी.सी आरक्षण के दायरे में नहीं आते हैं।"
+            },
+            "Other Backward Class (OBC - Non-Creamy Layer)":{
+                "en": (<><b>Other Backward Classes (OBC – Non-Creamy Layer) -</b> Reservation for eligible OBC candidates who fall 
+                    under the non-creamy layer.</>),
+                "hi": "अन्य पिछड़ा वर्ग (ओ.बी.सी – नॉन-क्रीमी लेयर) - नॉन-क्रीमी लेयर के अंतर्गत आने वाले पात्र ओ.बी.सी उम्मीदवारों के लिए आरक्षण।"
+            },
+            "Scheduled Caste (SC)":{
+                "en": (<><b>Scheduled Castes (SC) -</b> Reservation for candidates belonging to Scheduled Castes.</>),
+                "hi": "अनुसूचित जातियाँ (एस.सी) - अनुसूचित जातियों के उम्मीदवारों के लिए आरक्षण।"
+            },
+            "Scheduled Tribe (ST)":{
+                "en": (<><b>Scheduled Tribes (ST) -</b> Reservation for candidates belonging to Scheduled Tribes.</>),
+                "hi": "अनुसूचित जनजातियाँ (एस.टी) - अनुसूचित जनजाति के उम्मीदवारों के लिए आरक्षण।"
+            }
+        }
     }
 };
 
@@ -118,7 +158,7 @@ const PersonalInfo = ({ data }) =>{
             <Row>
                 <Col md={6}>
                     <div className="mtop15p">
-                        <Select name={PersonalInfoForm?.nationality?.id}  label={PersonalInfoForm?.nationality?.[lang+"Label"]} 
+                        <Select name={PersonalInfoForm?.nationality?.id} label={PersonalInfoForm?.nationality?.[lang+"Label"]} 
                             placeholder={PersonalInfoForm?.nationality?.[lang+"Placeholder"]}
                             value={eligibilityContextData?.data?.personalInfo?.nationality} fontSize="12"
                             options={PersonalInfoForm?.nationality?.[lang+"Options"]}
@@ -133,15 +173,17 @@ const PersonalInfo = ({ data }) =>{
                 </Col>
                 <Col md={6}>
                     <div className="mtop15p">
-                        <Select name="category" label="Your Category" placeholder="Select your Category" 
+                        <Select name={PersonalInfoForm?.category?.id} label={PersonalInfoForm?.category?.[lang+"Label"]} 
+                            placeholder={PersonalInfoForm?.category?.[lang+"Placeholder"]} 
                             value={eligibilityContextData?.data?.personalInfo?.category}
-                            options={["General (Unreserved / UR)","Economically Weaker Section (EWS)",
-                                "Other Backward Class (OBC - Non-Creamy Layer)",
-                                            "Scheduled Caste (SC)","Scheduled Tribe (ST)"]?.map((d,i)=>{
-                                                return { id: d, label: d, value: d };
-                                            })}
-                                            fontSize="12"
-                            validation={{ required:{ value: true, errorMessage:"This is a Mandatory Field" }}} />
+                            options={PersonalInfoForm?.category?.[lang+"Options"]} fontSize="12"
+                            validation={{ required:{ value: true, errorMessage:"This is a Mandatory Field" }}} 
+                            onChange={(event)=>setEligibilityContextData({...eligibilityContextData, 
+                                        "data":{ "personalInfo":{ "category": event?.target?.value } } })} />
+                        {(PersonalInfoForm?.category?.optionInfo?.[eligibilityContextData?.data?.personalInfo?.category]?.[lang]) 
+                            && (<div style={{ fontSize:'11px', marginTop:'8px' }}>
+                            <InfoIcon /> {PersonalInfoForm?.category?.optionInfo?.[eligibilityContextData?.data?.personalInfo?.category]?.[lang]}
+                        </div>)}
                     </div>
                 </Col>
             </Row>
