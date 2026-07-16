54f0576fe8dfe252b5b21a915827b5acf14425a9
NLNRao <nlnrao228>
2026-07-16 22:32:32 +0530

    Updated projects

--- c3/upn/src/components/exam-eligibility-calculator/components/step-03-relaxations/components/display-cseattempt/index.js
+++ c3/upn/src/components/exam-eligibility-calculator/components/step-03-relaxations/components/display-cseattempt/index.js
@@ -1,24 +1,59 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import { ContainerFluid, Row, Col, Select, Range, Switch, Button, Form } from "e-ui-react";
 import { getEligibilityContext } from "@Components/exam-eligibility-calculator/index.js";
+import { InfoIcon } from "@Components/exam-eligibility-calculator/commons.js";
+
+const DisplayCSEAttemptForm = {
+ "cseAttempt":{
+    "id":"cseAttempt",
+    "enLabel":"Have you appeared for UPSC Civil Services Examination before?",
+    "hiLabel":"क्या आप पहले यूपीएससी सिविल सेवा परीक्षा में शामिल हुए हैं?",
+    "enOptions":[{ id:'Yes', label:"Yes", value:"Yes"},
+                { id:'No', label:"No", value:"No" }],
+    "hiOptions":[{ id:'Yes', label:"हां", value:"Yes"},
+                { id:'No', label:"नहीं", value:"No" }]
+  },
+  "cseAttemptsUsed":{
+    "id":"cseAttemptsUsed",
+    "enLabel":"Attempts Used for CSE Examination?",
+    "hiLabel":"सीएसई परीक्षा के लिए कितने प्रयास किए जा सकते हैं?",
+    "enPlaceholder":"Select your Attempts",
+    "hiPlaceholder":"अपने प्रयास चुनें",
+    "enOptions":[{ id:'1_attempt', label: '1 Attempt', value:'1' },
+            { id:'2_attempts', label: '2 Attempts', value:'2' },
+            { id:'3_attempts', label: '3 Attempts', value:'3' },
+            { id:'4_attempts', label: '4 Attempts', value:'4' },
+            { id:'5_attempts', label: '5 Attempts', value:'5' },
+            { id:'6_attempts', label: '6 Attempts', value:'6' },
+            { id:'7_attempts', label: '7 Attempts', value:'7' },
+            { id:'8_attempts', label: '8 Attempts', value:'8' },
+            { id:'9_attempts', label: '9 Attempts', value:'9' }],
+    "hiOptions":[{ id:'1_attempt', label: '1  प्रयास', value:'1' },
+            { id:'2_attempts', label: '2 प्रयास', value:'2' },
+            { id:'3_attempts', label: '3 प्रयास', value:'3' },
+            { id:'4_attempts', label: '4 प्रयास', value:'4' },
+            { id:'5_attempts', label: '5 प्रयास', value:'5' },
+            { id:'6_attempts', label: '6 प्रयास', value:'6' },
+            { id:'7_attempts', label: '7 प्रयास', value:'7' },
+            { id:'8_attempts', label: '8 प्रयास', value:'8' },
+            { id:'9_attempts', label: '9 प्रयास', value:'9' }]
+  }
+};
 
 const DisplayCSEAttempt = () =>{
+  const { lang } = useParams();
   const { eligibilityContextData, setEligibilityContextData } = getEligibilityContext();
   return (<>
   <Row>
     <Col md={12}>
         <div className="mt-3">
-            <Switch type="radio" id="cseAttempt" name="cseAttempt"  layout="horizontal"
-                label="Have you appeared for UPSC Civil Services Examination before??" 
+            <Switch type="radio" id={DisplayCSEAttemptForm?.cseAttempt?.id} 
+                name={DisplayCSEAttemptForm?.cseAttempt?.id}  layout="horizontal"
+                label={DisplayCSEAttemptForm?.cseAttempt?.[lang+"Label"]} 
                 value={eligibilityContextData?.data?.reservations?.cseAttempt}
-                options={[{ id:'Yes', label:"Yes", value:"Yes"},
-                    { id:'No', label:"No", value:"No" }]} 
-                validation={{
-                    required:{
-                        value: true,
-                        errorMessage:"[This is a Mandatory Field]"
-                    } 
-                }} />
+                options={DisplayCSEAttemptForm?.cseAttempt?.[lang+"Options"]} 
+                validation={{ required:{ value: true, errorMessage:"[This is a Mandatory Field]" } }} />
         </div>
     </Col>
   </Row>
@@ -26,17 +61,12 @@ const DisplayCSEAttempt = () =>{
             <Row>
             <Col md={6}>
                 <div className="mt-3">
-                    <Select name="cseAttemptsUsed" label="Attempts Used for CSE Examination" placeholder="Select Completed Attempts"
+                    <Select name={DisplayCSEAttemptForm?.cseAttemptsUsed?.id} 
+                    label={DisplayCSEAttemptForm?.cseAttemptsUsed?.[lang+"Label"]}  
+                    placeholder={DisplayCSEAttemptForm?.cseAttemptsUsed?.[lang+"Placeholder"]}  
                     value={eligibilityContextData?.data?.reservations?.cseAttemptsUsed}
-                    options={["1","2","3","4","5","6","7","8","9"]?.map((d,i)=>{
-                        return { id: d, label: d, value: d };
-                    })} 
-                    validation={{
-                        required:{
-                            value: true,
-                            errorMessage:"[This is a Mandatory Field]"
-                        } 
-                    }} />
+                    options={DisplayCSEAttemptForm?.cseAttemptsUsed?.[lang+"Options"]} 
+                    validation={{ required:{ value: true, errorMessage:"[This is a Mandatory Field]" } }} />
                 </div>
             </Col>
     </Row>
--- c3/upn/src/components/exam-eligibility-calculator/components/step-03-relaxations/index.js
+++ c3/upn/src/components/exam-eligibility-calculator/components/step-03-relaxations/index.js
@@ -7,39 +7,17 @@ import DisplayExServiceMan from "./components/display-exserviceman/index.js";
 import DisplayDefencePersonnelDisabled from "./components/display-defenceperdisabled/index.js";
 import DisplayCSEAttempt from "./components/display-cseattempt/index.js";
 
-const RelaxationForm = {
-   "cseAttempt":{
-                "id":"cseAttempt",
-                "enLabel":"Have you appeared for UPSC Civil Services Examination before?",
-                "hiLabel":"क्या आप पहले यूपीएससी सिविल सेवा परीक्षा में शामिल हुए हैं?",
-                "enOptions":[{ id:'Yes', label:"Yes", value:"Yes"},
-                            { id:'No', label:"No", value:"No" }],
-                "hiOptions":[{ id:'Yes', label:"हां", value:"Yes"},
-                            { id:'No', label:"नहीं", value:"No" }]
-            },
-   "cseAttemptsUsed":{
-                "id":"cseAttemptsUsed",
-                "enLabel":"Attempts Used for CSE Examination?",
-                "hiLabel":"सीएसई परीक्षा के लिए कितने प्रयास किए जा सकते हैं?",
-                "enOptions":[{ id:'1_attempt', label: '1 Attempt', value:'1' },
-                             { id:'2_attempts', label: '2 Attempts', value:'2' },
-                             { id:'3_attempts', label: '3 Attempts', value:'3' },
-                             { id:'4_attempts', label: '4 Attempts', value:'4' },
-                             { id:'5_attempts', label: '5 Attempts', value:'5' },
-                             { id:'6_attempts', label: '6 Attempts', value:'6' },
-                             { id:'7_attempts', label: '7 Attempts', value:'7' },
-                             { id:'8_attempts', label: '8 Attempts', value:'8' },
-                             { id:'9_attempts', label: '9 Attempts', value:'9' }],
-                "hiOptions":[{ id:'1_attempt', label: '1  प्रयास', value:'1' },
-                             { id:'2_attempts', label: '2 प्रयास', value:'2' },
-                             { id:'3_attempts', label: '3 प्रयास', value:'3' },
-                             { id:'4_attempts', label: '4 प्रयास', value:'4' },
-                             { id:'5_attempts', label: '5 प्रयास', value:'5' },
-                             { id:'6_attempts', label: '6 प्रयास', value:'6' },
-                             { id:'7_attempts', label: '7 प्रयास', value:'7' },
-                             { id:'8_attempts', label: '8 प्रयास', value:'8' },
-                             { id:'9_attempts', label: '9 प्रयास', value:'9' }]
-            }
+const RelaxationsForm = {
+  "enDesc": (<div style={{ fontSize:'12px' }}>Provide additional information, if applicable. These details help calculate age relaxation, attempt limits, and eligibility accurately.</div>),
+  "hiDesc": (<div style={{ fontSize:'14px' }}>अगर लागू हो, तो अतिरिक्त जानकारी दें। ये विवरण उम्र में छूट, प्रयास की सीमा और पात्रता की सही-सही गणना करने में मदद करते हैं।</div>),
+  "nextBtn":{
+        "en":"Next Step",
+        "hi":"अगला कदम"
+    },
+    "resetBtn":{
+        "en":"Reset Form",
+        "hi":"फ़ॉर्म रीसेट करें"
+    }
 };
 
 const Relaxations = ({ data }) =>{
@@ -53,19 +31,18 @@ const Relaxations = ({ data }) =>{
  return (<div>
     <div><h5><b>3. {data?.[lang+"Label"]}</b></h5><hr/></div>
     <Form name="ReservationsAndRelaxations"  
-        btnSubmit={{ align: 'right', btnType:'success', label:(<b>Next</b>), size: 12 }} 
-        btnReset={{ btnType:'danger', label:(<b>Reset</b>), size: 11 }}
+        btnSubmit={{ align: 'right', btnType:'success', label:(<b>{RelaxationsForm?.nextBtn?.[lang]}</b>), size: 12 }} 
+        btnReset={{ btnType:'danger', label:(<b>{RelaxationsForm?.resetBtn?.[lang]}</b>), size: 11 }}
         onSubmit={NextHandler}>
     <ContainerFluid>
         <Row>
             <Col md={12}>
-                <div className="mt-2" style={{ fontSize:'13px' }}><p>Provide additional information, if applicable. These details help 
-                    calculate age relaxation, attempt limits, and eligibility accurately.</p></div>
+                <div className="mt-2">{RelaxationsForm?.[lang+"Desc"]}</div>
             </Col>
         </Row>
         <div className="list-group">
             <div className="list-group-item" style={{ backgroundColor:'#eee' }}>
-                <DisplayPwBD data={RelaxationForm?.pwBD} />
+                <DisplayPwBD />
             </div>
             <div className="list-group-item" style={{ backgroundColor:'#eee' }}>
                 <DisplayExServiceMan />
