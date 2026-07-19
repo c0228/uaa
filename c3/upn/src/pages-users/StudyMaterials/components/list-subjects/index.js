8d922455b9b0727b39ec04535b62de69d18e939f
NLNRao <nlnrao228>
2026-07-19 23:47:30 +0530

    Updated projects

--- c3/upn/src/pages-users/StudyMaterials/components/list-subjects/index.js
+++ c3/upn/src/pages-users/StudyMaterials/components/list-subjects/index.js
@@ -1,33 +1,54 @@
 import React from "react";
 import { ContainerFluid, Row, Col, Card, Icon, Badge, Button } from "e-ui-react";
+import { useParams } from "react-router-dom";
 
 const SubjectsList = ({ data }) =>{
+ const { lang } = useParams();
+ const sections = data && Object.keys(data);
  return (<div>
-    <div><h4><b>Indian Polity</b></h4><hr/></div>
-    <div className="mtop15p">
-        <Row>
-        <Col md={6}>
-        {/* */}
-            <Card padding={15} backgroundColor="#fffdfd" style={{ border:'1px solid #000'}}>
+    {sections?.map((section,i1)=>{
+        const label = data?.[section]?.[lang+"Label"];
+        const icon = data?.[section]?.icon;
+        const subjects = data?.[section]?.subjects;
+        const subjectList = subjects && Object.keys(subjects);
+        console.log("subjects: ", subjects);
+        return (<div key={i1}>
+        <div><h4><b>{label}</b></h4><hr/></div>
+            <div className="mtop15p">
+            <Row>
+                {subjectList?.map((s,i2)=>{
+                    const icon = subjects[s]?.icon;
+                    const label = subjects[s]?.[lang+"Label"];
+                    const chapters = subjects[s]?.chapters;
+                    const topics = subjects[s]?.topics;
+                    const studyHours = subjects[s]?.study_hours;
+                    const tags = subjects[s]?.[lang+"Tags"];
+                    return (<Col md={6}>
+                {/* */}
+                <Card padding={15} backgroundColor="#fffdfd" style={{ border:'1px solid #000'}}>
                 <div style={{ display:'flex', flex:1 }}>
                     <div style={{ width:'20%', display: 'flex', justifyContent:'center', alignItems:'center' }}>
                         <div align="center" style={{ border:'1px solid #000', padding:'15px', borderRadius:'8px' }}>
-                            <Icon type="FontAwesome" name="fa-university" size={42} />
+                            <Icon type="FontAwesome" name={icon} size={42} />
                         </div>
                     </div>
                     <div style={{ width:'80%', paddingLeft:'15px' }}>
-                        <div><h5><b>The Constitution of India</b></h5></div>
-                        <div style={{ color:'#555' }}>24 Chapters | 184 Topics</div>
+                        <div><h5><b>{label}</b></h5></div>
+                        <div style={{ color:'#555' }}>
+                            {lang==='hi'?(<>{chapters} अध्याय | {topics} विषय</>):
+                                (<>{chapters} Chapters | {topics} Topics</>)} 
+                        </div>
                         <div style={{ marginTop:'12px', fontSize:'12px' }}>
                             <span style={{ color:'#000', border:'1px solid #000', backgroundColor:'#eee', padding:'5px', borderRadius:'8px' }}>
                                 <Icon type="FontAwesome" name="fa-clock-o" size={13} style={{ marginRight:'5px' }} />
-                                Est. <b>220</b> Study Hours
+                                 {lang==='hi'?(<>अनुमानित. <b>{studyHours}</b> पढ़ाई के घंटे</>):(<>Est. <b>{studyHours}</b> Study Hours</>)}
+                                
                             </span>
                         </div>
                     </div>
                 </div>
                 <div className="mtop10p">
-                    {["CAPF", "CDS", "CGSE", "CMS", "CSE", "ESE", "IES/ISS", "IFoS", "NDA", "RT", "SCRA"]?.map((e,i)=>{
+                    {tags?.map((e,i)=>{
                        /* const colors= [{ dark:'#ec3d0e', light:'#ffd9d9' },
                                 { dark:'#065fc5', light:'#dcecff' },
                                 { dark:'#07850c', light:'#e0ffe1' },
@@ -46,14 +67,17 @@ const SubjectsList = ({ data }) =>{
                 <div align="right" className="mtop5p">
                     <Button type="dark" size={12}>
                         <Icon type="FontAwesome" name="fa-play-circle" size={13} style={{ marginRight:'5px' }} />
-                        <b>Start Learning</b>   
+                        <b>{lang==='hi'?'सीखना शुरू करें':'Start Learning'}</b>   
                     </Button>
                 </div>
             </Card>
-        {/* */}
-        </Col>
-        </Row>
-    </div>
+            {/* */}
+            </Col>);
+                })}
+            </Row>
+        </div>
+        </div>);
+    })}
  </div>);
 };
   export default SubjectsList;
