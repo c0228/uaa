733d05d1f583d397e9e3a6f4359e649db3e7f89c
NLNRao <nlnrao228>
2026-08-21 18:32:40 +0530

    Updated Projects

--- c3/mlh/src/pages/ProfileSetup/SetPostalCode/index.js
+++ c3/mlh/src/pages/ProfileSetup/SetPostalCode/index.js
@@ -1,152 +1,11 @@
 import React, { useState, useEffect, useRef } from "react";
 import { ContainerFluid, Row, Col } from "e-ui-react";
+import MapPostalCode from "@Components/map-postal-code/index.js";
 import Header2 from "@Templates/Header2/index.js";
-import L from "leaflet";
-import "leaflet/dist/leaflet.css";
-import markerIcon from "leaflet/dist/images/marker-icon.png";
-import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
-import markerShadow from "leaflet/dist/images/marker-shadow.png";
 import './index.css';
 
-// Hard-coded PIN code
-    const PINCODE = "500036";
-
-    // Hard-coded boundary for testing
-    // GeoJSON uses [longitude, latitude]
-    const PINCODE_BOUNDARY = {
-        type: "Feature",
-        properties: {
-            pincode: PINCODE
-        },
-        geometry: {
-            type: "Polygon",
-            coordinates: [[
-                [78.0000, 17.0000],
-                [78.0500, 17.0000],
-                [78.0600, 17.0500],
-                [78.0200, 17.0900],
-                [77.9800, 17.0600],
-                [78.0000, 17.0000]
-            ]]
-        }
-    };
-
-    // Fix Leaflet marker icon
-    const DefaultIcon = L.icon({
-        iconUrl: markerIcon,
-        iconRetinaUrl: markerIcon2x,
-        shadowUrl: markerShadow,
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowSize: [41, 41]
-    });
-
-
 const SetPostalCode = ()=>{
- const mapRef = useRef(null);
- const boundaryRef = useRef(null);
- const markerRef = useRef(null);
  const [userDetails, setUserDetails] = useState();
- L.Marker.prototype.options.icon = DefaultIcon;
- const initMap = () =>{
-    const map = L.map("map").setView([20.5937, 78.9629], 5);
-        L.tileLayer(
-            "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-            {
-                attribution: "&copy; OpenStreetMap contributors"
-            }
-        ).addTo(map);
-
-        mapRef.current = map;
-
-        return () => {
-            map.remove();
-        };
- };
-
- const showPincode = (map, pincode, geoJSON) => {
-
-        // Remove previous boundary
-        if (boundaryRef.current) {
-            map.removeLayer(boundaryRef.current);
-        }
-
-        // Remove old marker
-        if (markerRef.current) {
-            map.removeLayer(markerRef.current);
-        }
-
-        // Create boundary
-        const boundary = L.geoJSON(
-            geoJSON,
-            {
-                style: {
-                    color: "#2196f3",
-                    weight: 3,
-                    fillColor: "#2196f3",
-                    fillOpacity: 0.20
-                }
-            }
-        ).addTo(map);
-
-        // Zoom to boundary
-        map.fitBounds(
-            boundary.getBounds(),
-            {
-                padding: [30, 30]
-            }
-        );
-
-        // Center of PIN-code boundary
-    const center = boundary.getBounds().getCenter();
-
-    // Add marker
-    const marker = L.marker(center)
-        .addTo(map);
-
-        // Bind popup and OPEN it immediately
-        boundary.bindPopup(
-            `<b>PIN Code:</b> ${pincode}`
-        );
-
-        boundaryRef.current = boundary;
-         markerRef.current = marker;
-    };
-
- const getPincodeBoundary = async (pincode) => {
-
-    const url =
-        `https://livingatlas.esri.in/server1/rest/services/India/Pincode_Boundary_2025/MapServer/0/query` +
-        `?where=pin_code%3D%27${pincode}%27` +
-        `&outFields=*` +
-        `&returnGeometry=true` +
-        `&f=geojson`;
-
-    const response = await fetch(url);
-
-    if (!response.ok) {
-        throw new Error("Failed to get PIN boundary");
-    }
-
-    return await response.json();
-};
-
- const handleShowPincode = async() => {
-
-        if (!mapRef.current) {
-            return;
-        }
-
-        const geoJSON = await getPincodeBoundary(PINCODE);
-
-        showPincode(
-            mapRef.current,
-            PINCODE,
-            geoJSON
-        );
-    };
-
  const initialize = () =>{
     const storedUser = localStorage.getItem("MLH_USER_DETAILS");
     if(!storedUser) {
@@ -163,9 +22,7 @@ const SetPostalCode = ()=>{
     }
  };
  useEffect(()=>{
-    const cleanupMap = initMap();
     initialize();
-    return cleanupMap;
  },[]);
  return (<>
     <Header2 />
@@ -182,10 +39,9 @@ const SetPostalCode = ()=>{
                             <div>{userDetails?.email}</div>
                         </div>
                 </div>
-                <button  onClick={handleShowPincode}>Press</button>
             </Col>
             <Col md={9}>
-                <div id="map" style={{ position:'fixed', width: "100%", height: screen.height, border:'1px solid #ccc' }} />
+                <MapPostalCode pincode="" />
             </Col>
         </Row>
     </ContainerFluid>
