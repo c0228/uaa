d82a082da004d52cd7b0f4ce2910734199ab1866
NLNRao <nlnrao228>
2026-08-27 22:18:26 +0530

    Updated projects

---
+++ c3/geocer/src/services/allVillages.js
@@ -0,0 +1,15 @@
+import { ReadExcel } from "#Utils/ExcelManager.js";
+
+const PROJECT_ROOT = process.cwd();
+
+const OUTPUT_FILE = PROJECT_ROOT+"\\output\\allVillages.txt";
+const INPUT_FILE = PROJECT_ROOT+"\\data\\allVillages.xlsx";
+
+async function businesslogic(values, output) {
+
+ const query = "";
+ console.log(query);
+ output.write(query+"\n");
+}
+
+ReadExcel(INPUT_FILE, OUTPUT_FILE, businesslogic).catch(console.error);
